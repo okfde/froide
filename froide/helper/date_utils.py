0f6e5cf2ede6112d29e3119170e1eb633aad245f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2011-04-20 21:43:02 +0200

    Correct offset calculation?

--- froide/helper/date_utils.py
+++ froide/helper/date_utils.py
@@ -7,7 +7,7 @@ PYTZ_TIME_ZONE = pytz.timezone(settings.TIME_ZONE)
 
 def convert_to_local(date, offset_in_seconds=None):
     if offset_in_seconds is not None:
-        date = date + timedelta(seconds=offset_in_seconds)
+        date = date - timedelta(seconds=offset_in_seconds)
     date = pytz.utc.localize(date)
     return date.astimezone(PYTZ_TIME_ZONE)
 
