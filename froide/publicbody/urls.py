84c824c759d1fbd6ef0a0e07339d815a8726666e
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2011-04-20 21:42:04 +0200

    mail message letter_start and letter_end from chosen law

--- froide/publicbody/urls.py
+++ froide/publicbody/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns("",
     url(r"^autocomplete/$", "publicbody.views.autocomplete", name="publicbody-autocomplete"),
     url(r"^search/$", "publicbody.views.search", name="publicbody-search"),    url(r"^(?P<pk>[\d+]).(?P<format>json)$", PublicBodyDetailView.as_view(), name="publicbody-show-json"),
     url(r"^(?P<slug>[-\w]+)$", PublicBodyDetailView.as_view(), name="publicbody-show"),
+    url(r"^(?P<pk>\d+).(?P<format>json)$", PublicBodyDetailView.as_view(), name="publicbody-show-json"),
     url(r"^(?P<slug>[-\w]+).(?P<format>json)$", PublicBodyDetailView.as_view(), name="publicbody-show-json"),
 )
