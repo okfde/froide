ec8bd4dba6048c2db612e78fcbed3fda31c894ca
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2011-04-27 10:03:59 +0200

    Create admin classes for FoiEvent and FoiAttachment

--- froide/foirequest/admin.py
+++ froide/foirequest/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from froide.foirequest.models import FoiRequest, FoiMessage, FoiAttachment
+from froide.foirequest.models import (FoiRequest, FoiMessage,
+        FoiAttachment, FoiEvent, PublicBodySuggestion)
+
 
 class FoiMessageInline(admin.TabularInline):
     model = FoiMessage
@@ -11,18 +13,31 @@ class FoiRequestAdmin(admin.ModelAdmin):
         FoiMessageInline,
     ]
 
+
 class FoiAttachmentInline(admin.TabularInline):
     model = FoiAttachment
+
 
 class FoiMessageAdmin(admin.ModelAdmin):
     inlines = [
         FoiAttachmentInline,
     ]
 
+
 class FoiAttachmentAdmin(admin.ModelAdmin):
+    pass
+
+
+class FoiEventAdmin(admin.ModelAdmin):
+    pass
+
+
+class PublicBodySuggestionAdmin(admin.ModelAdmin):
     pass
 
 
 admin.site.register(FoiRequest, FoiRequestAdmin)
 admin.site.register(FoiMessage, FoiMessageAdmin)
 admin.site.register(FoiAttachment, FoiAttachmentAdmin)
+admin.site.register(FoiEvent, FoiEventAdmin)
+admin.site.register(PublicBodySuggestion, PublicBodySuggestionAdmin)
