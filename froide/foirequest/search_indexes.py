d33d7e5bf29d8c135c68eb5f1206d2f7df6f42ed
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2011-05-04 17:32:50 +0200

    Add URL and description to search index of FoiRequest

--- froide/foirequest/search_indexes.py
+++ froide/foirequest/search_indexes.py
@@ -7,10 +7,11 @@ from foirequest.models import FoiRequest
 class FoiRequestIndex(indexes.SearchIndex):
     text = indexes.EdgeNgramField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
+    description = indexes.CharField(model_attr='description')
     status = indexes.CharField(model_attr='status')
     first_message = indexes.DateTimeField(model_attr='first_message')
     last_message = indexes.DateTimeField(model_attr='last_message')
-    
+    url = indexes.CharField(model_attr='get_absolute_url')
 
     def get_queryset(self):
         """Used when the entire index for model is updated."""
