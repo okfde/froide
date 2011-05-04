0202eeed429149cbfafd53d9ba6281a0926ea9df
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2011-05-04 17:29:45 +0200

    Add labels to account forms and add a NewUserWithPasswordForm that adds password inputs to the new user form.

--- froide/account/forms.py
+++ froide/account/forms.py
@@ -7,11 +7,16 @@ from django.contrib.auth.models import User
 from helper.widgets import EmailInput
 
 class NewUserForm(forms.Form):
-    first_name = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'placeholder': _('First Name'), 'class': 'inline'}))
-    last_name = forms.CharField(max_length=30, widget=forms.TextInput(
-        attrs={'placeholder': _('Last Name'), 'class': 'inline'}))
-    user_email = forms.EmailField(widget=EmailInput(
-        attrs={'placeholder': _('mail@ddress.net')}))
+    first_name = forms.CharField(max_length=30,
+            label=_('First name'),
+            widget=forms.TextInput(attrs={'placeholder': _('First Name'),
+                'class': 'inline'}))
+    last_name = forms.CharField(max_length=30,
+            label=_('Last name'),
+            widget=forms.TextInput(attrs={'placeholder': _('Last Name'),
+                'class': 'inline'}))
+    user_email = forms.EmailField(label=_('Email address'),
+            widget=EmailInput(attrs={'placeholder': _('mail@ddress.net')}))
 
     def clean_first_name(self):
         return self.cleaned_data['first_name'].strip()
@@ -31,7 +36,21 @@ class NewUserForm(forms.Form):
 
         return email
 
+class NewUserWithPasswordForm(NewUserForm):
+    password = forms.CharField(widget=forms.PasswordInput,
+            label=_('Password'))
+    password2 = forms.CharField(widget=forms.PasswordInput,
+            label=_('Password (repeat)'))
+
+    def clean(self):
+        cleaned = super(NewUserWithPasswordForm, self).clean()
+        if cleaned['password'] != cleaned['password2']:
+            raise forms.ValidationError(_("Passwords do not match!"))
+        return cleaned
+
 class UserLoginForm(forms.Form):
     email = forms.EmailField(widget=EmailInput(
-        attrs={'placeholder': _('mail@ddress.net')}))
-    password = forms.CharField(widget=forms.PasswordInput)
+        attrs={'placeholder': _('mail@ddress.net')}),
+        label=_('Email address'))
+    password = forms.CharField(widget=forms.PasswordInput,
+        label=_('Password'))
