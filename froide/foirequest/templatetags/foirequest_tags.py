de1d9cdec78d86a2c4e754102b029af7c30b188a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2011-07-11 00:35:29 +0200

    Highlight real request among legal boiler plate on request page

---
+++ froide/foirequest/templatetags/foirequest_tags.py
@@ -0,0 +1,19 @@
+from django import template
+from django.utils.safestring import mark_safe
+from django.utils.html import escape
+
+
+register = template.Library()
+
+def highlight_request(message):
+    content = message.get_content()
+    description = message.request.description
+    try:
+        index = content.index(description)
+    except ValueError:
+        return content
+    offset = index + len(description)
+    return mark_safe('%s<div class="highlight">%s</div>%s' % (escape(content[:index]),
+            escape(description), escape(content[offset:])))
+
+register.simple_tag(highlight_request)
