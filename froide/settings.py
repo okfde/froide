0476d2cc25992a8de367673d2e9fd63df520e032
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2011-07-14 22:16:08 +0200

    Remove German DATE_FORMAT and DATE_INPUT_FORMATS from settings

--- froide/settings.py
+++ froide/settings.py
@@ -49,9 +49,6 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-DATE_FORMAT = "d.m.Y."
-DATE_INPUT_FORMATS = ("%d.%m.%Y",)
-
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, "..", "files")
