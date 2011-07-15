a337da485056f050b9abaca917f9771fd783c6d0
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2011-07-15 21:31:17 +0200

    Add command to count Public Bodies for PublicBodyTopics and store that count

---
+++ froide/publicbody/management/commands/count_topic.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.utils import translation
+from django.conf import settings
+
+class Command(BaseCommand):
+    help = "Counts PulbicBodies for Topics"
+
+    def handle(self, *args, **options):
+        from publicbody.models import PublicBodyTopic
+        translation.activate(settings.LANGUAGE_CODE)
+        for topic in PublicBodyTopic.objects.all():
+            topic.count = len(topic.publicbody_set.all())
+            topic.save()
+            self.stdout.write("%s: %d\n" % (topic.name, topic.count))
