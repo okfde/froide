5436754a9f74c71b563ebb79909cfb079dbfe90b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2011-08-08 15:42:00 +0200

    replace _ with pgettext in public body url

--- froide/publicbody/urls.py
+++ froide/publicbody/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 from django.utils.translation import ugettext as _
+from django.utils.translation import pgettext
 
 from haystack.query import SearchQuerySet
 from haystack.views import SearchView, search_view_factory
@@ -24,7 +25,8 @@ urlpatterns += patterns("",
     url(r"^autocomplete/$", "publicbody.views.autocomplete", name="publicbody-autocomplete"),
     # Translators: part in Public Body URL
     url(r"^%s/json/$" % _('search'), "publicbody.views.search_json", name="publicbody-search_json"),
-    url(r"^%s/(?P<topic>[-\w]+)$" % _('topic'), 'publicbody.views.show_topic', name="publicbody-show_topic"),
+    # Translators: part in Public Body URL
+    url(r"^%s/(?P<topic>[-\w]+)$" % pgettext('URL part', 'topic'), 'publicbody.views.show_topic', name="publicbody-show_topic"),
     url(r"^(?P<pk>[\d+]).(?P<format>json)$", PublicBodyDetailView.as_view(), name="publicbody-show-json"),
     url(r"^(?P<slug>[-\w]+)$", PublicBodyDetailView.as_view(), name="publicbody-show"),
     url(r"^(?P<pk>\d+).(?P<format>json)$", PublicBodyDetailView.as_view(), name="publicbody-show_json"),
