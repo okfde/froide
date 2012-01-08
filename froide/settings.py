8d894e0f3744602700d4902ded8d917953c65e68
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-01-08 23:23:27 +0100

    Add USE_TZ = True to settings file

--- froide/settings.py
+++ froide/settings.py
@@ -34,6 +34,7 @@ DATABASES = {
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
 TIME_ZONE = 'Europe/Berlin'
+USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
