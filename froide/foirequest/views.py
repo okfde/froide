e14a99d6646c7785c462f250d2e263735766580c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-03-30 17:19:32 +0200

    Add tracking beacon after successful request submission

--- froide/foirequest/views.py
+++ froide/foirequest/views.py
@@ -314,7 +314,7 @@ def submit_request(request, public_body=None):
             else:
                 messages.add_message(request, messages.INFO,
                     _('Your request has been sent.'))
-            return HttpResponseRedirect(foi_request.get_absolute_url())
+            return HttpResponseRedirect(foi_request.get_absolute_url() + _('?request-made'))
         else:
             AccountManager(user).send_confirmation_mail(request_id=foi_request.pk,
                     password=password)
