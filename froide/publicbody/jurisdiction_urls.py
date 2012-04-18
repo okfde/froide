2b999e01e6c74920634aaa8544f07d6170ba6aaf
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-04-19 01:14:33 +0200

    Add banner for NRW launch, add press release, laws, public body count and requests to NRW page.

--- froide/publicbody/jurisdiction_urls.py
+++ froide/publicbody/jurisdiction_urls.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext as _
 
 
 urlpatterns = patterns("publicbody.views",
-    url(r"^$", 'show_jurisdiction', name="publicbody-show-jurisdiction"),
+    url(r"^$", 'show_jurisdiction', name="publicbody-show_jurisdiction"),
     # Translators: URL part
     url(r"^%s/$" % _('entity'), 'show_pb_jurisdiction',
             name="publicbody-show-pb_jurisdiction"),
