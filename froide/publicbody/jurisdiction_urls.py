ee4b84d99ead22505ea78eecc677f75dfa10107d
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-05-10 15:40:14 +0200

    Change publicbody-show-pb_jurisdiction URL and redirect old one

--- froide/publicbody/jurisdiction_urls.py
+++ froide/publicbody/jurisdiction_urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls.defaults import patterns, url
-
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext as _
 
 
 urlpatterns = patterns("publicbody.views",
     url(r"^$", 'show_jurisdiction', name="publicbody-show_jurisdiction"),
+# Translators: URL part
+    url(r"^%s/$" % _('entity'),
+        lambda r, slug: HttpResponseRedirect(
+            reverse("publicbody-show-pb_jurisdiction", kwargs={'slug': slug}))),
     # Translators: URL part
-    url(r"^%s/$" % _('entity'), 'show_pb_jurisdiction',
+    url(r"^%s/$" % _('entities'), 'show_pb_jurisdiction',
             name="publicbody-show-pb_jurisdiction"),
 )
