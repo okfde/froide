185571932f518760bb3045347578caa98ef820f5
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-05-15 20:50:33 +0200

    Use manager method for view articles

--- froide/foiidea/views.py
+++ froide/foiidea/views.py
@@ -5,5 +5,5 @@ from foiidea.models import Article
 
 def index(request):
     return render(request, 'foiidea/index.html', {
-        'object_list': Article.objects.all().select_related('public_bodies', 'foirequests')
+        'object_list': Article.objects.get_ordered()
     })
