3c0b64b284df8b1bd384553199dc995e9752bb45
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-05-22 21:41:33 +0200

    Add score and rank to foiidea article admin list_display

--- froide/foiidea/admin.py
+++ froide/foiidea/admin.py
@@ -9,7 +9,7 @@ class SourceAdmin(admin.ModelAdmin):
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'source', 'date', 'order')
+    list_display = ('title', 'source', 'date', 'score', 'rank', 'order')
     raw_id_fields = ['public_bodies', 'foirequests']
 
 admin.site.register(Source, SourceAdmin)
