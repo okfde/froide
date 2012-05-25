febe2c6c6947b85f5e6217818e1335be5be27467
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-05-25 18:27:31 +0200

    Add URL and templates for individual articles

--- froide/foiidea/urls.py
+++ froide/foiidea/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns("",
     (r'^$', 'foiidea.views.index', {}, 'foiidea-index'),
+    (r'^(?P<article_id>\d+)/$', 'foiidea.views.show', {}, 'foiidea-show'),
 )
--- froide/foiidea/views.py
+++ froide/foiidea/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from foiidea.models import Article
 
@@ -6,4 +6,10 @@ from foiidea.models import Article
 def index(request):
     return render(request, 'foiidea/index.html', {
         'object_list': Article.objects.get_ordered()
+    })
+
+
+def show(request, article_id):
+    return render(request, 'foiidea/show.html', {
+        'object': get_object_or_404(Article, id=int(article_id))
     })
