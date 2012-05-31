f666252c6aa77ddbc078a9dfb3dfd1f60054b473
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-05-31 18:55:47 +0200

    Import Model late in management command

--- froide/foiidea/management/commands/recalculate_idea_rank.py
+++ froide/foiidea/management/commands/recalculate_idea_rank.py
@@ -2,12 +2,11 @@ from django.core.management.base import BaseCommand
 from django.utils import translation
 from django.conf import settings
 
-from foiidea.models import Article
-
 
 class Command(BaseCommand):
     help = "Recalculate Article Order"
 
     def handle(self, **options):
         translation.activate(settings.LANGUAGE_CODE)
+        from foiidea.models import Article
         Article.objects.recalculate_order()
