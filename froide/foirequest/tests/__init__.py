c8a5adda805dcf4baf89ec8f2670adb463825a6e
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-06-25 21:35:51 +0200

    Import all foirequest testcases to module

--- froide/foirequest/tests/__init__.py
+++ froide/foirequest/tests/__init__.py
@@ -1,16 +1,3 @@
-import unittest
-import test_mail
-import test_request
-import test_web
-
-__tests__ = [test_mail, test_request, test_web]
-
-
-def suite():
-    suite = unittest.TestSuite()
-    tests = []
-    for test in __tests__:
-        tl = unittest.TestLoader().loadTestsFromModule(test)
-        tests += tl._tests
-    suite._tests = tests
-    return suite
+from .test_mail import *
+from .test_request import *
+from .test_web import *
