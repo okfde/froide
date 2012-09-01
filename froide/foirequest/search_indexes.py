6bc48a32a63c3d1c30636a82ef510df7f45dec73
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-09-01 19:01:07 +0200

    Switch to using celery-haystack

--- froide/foirequest/search_indexes.py
+++ froide/foirequest/search_indexes.py
@@ -1,12 +1,11 @@
 from haystack import indexes
-from haystack import site
 
-from froide.helper.searchindex import QueuedRealTimeSearchIndex
+from celery_haystack.indexes import CelerySearchIndex
 
 from .models import FoiRequest
 
 
-class FoiRequestIndex(QueuedRealTimeSearchIndex):
+class FoiRequestIndex(CelerySearchIndex, indexes.Indexable):
     text = indexes.EdgeNgramField(document=True, use_template=True)
     title = indexes.CharField(model_attr='title')
     description = indexes.CharField(model_attr='description')
@@ -18,9 +17,9 @@ class FoiRequestIndex(QueuedRealTimeSearchIndex):
     url = indexes.CharField(model_attr='get_absolute_url')
     public_body_name = indexes.CharField(model_attr='public_body__name', default="")
 
+    def get_model(self):
+        return FoiRequest
+
     def index_queryset(self):
         """Used when the entire index for model is updated."""
-        return FoiRequest.published.get_for_search_index()
-
-
-site.register(FoiRequest, FoiRequestIndex)
+        return self.get_model().published.get_for_search_index()
