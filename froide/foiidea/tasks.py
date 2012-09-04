6ba8e942edaf424c7b20983a5e829736c38b8110
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-09-04 11:20:56 +0200

    Remove complex exception mechanism for celery task

--- froide/foiidea/tasks.py
+++ froide/foiidea/tasks.py
@@ -1,10 +1,7 @@
-import sys
-
 from celery.task import task
 
 from django.conf import settings
 from django.utils import translation
-from django.db import transaction
 
 from .crawler import crawl_source_by_id
 from .models import Article
@@ -13,21 +10,7 @@ from .models import Article
 @task
 def fetch_articles(source_id):
     translation.activate(settings.LANGUAGE_CODE)
-
-    def run(source_id):
-        try:
-            crawl_source_by_id(int(source_id))
-        except Exception:
-            transaction.rollback()
-            return sys.exc_info()
-        else:
-            transaction.commit()
-            return None
-    run = transaction.commit_manually(run)
-    exc_info = run(source_id)
-    if exc_info is not None:
-        from sentry.client.models import client
-        client.create_from_exception(exc_info=exc_info, view="froide.foiidea.tasks.fetch_articles")
+    crawl_source_by_id(int(source_id))
 
 
 @task
