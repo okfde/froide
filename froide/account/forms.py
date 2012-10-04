2c805c2700dc35628c25872cd369e51b3d58c894
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-10-04 21:40:40 +0200

    Make postal address optional in account form

--- froide/account/forms.py
+++ froide/account/forms.py
@@ -15,16 +15,18 @@ class NewUserForm(forms.Form):
     first_name = forms.CharField(max_length=30,
             label=_('First name'),
             widget=forms.TextInput(attrs={'placeholder': _('First Name'),
-                'class': 'inline'}))
+                'class': 'span2'}))
     last_name = forms.CharField(max_length=30,
             label=_('Last name'),
             widget=forms.TextInput(attrs={'placeholder': _('Last Name'),
-                'class': 'inline'}))
-    address = forms.CharField(max_length=300, min_length=10,
+                'class': 'span2'}))
+    address = forms.CharField(max_length=300,
+            required=False,
             label=_('Mailing Address'),
-            help_text=_('Your address will not be displayed publicly.'),
+            help_text=_('Your address will not be displayed publicly and is only needed in case a public body needs to send you paper.'),
             widget=forms.Textarea(attrs={
                 'rows': '3',
+                'class': 'span3',
                 'placeholder': _('Street, Post Code, City'),
             }))
     user_email = forms.EmailField(label=_('Email address'),
