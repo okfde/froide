7a2106b8249eb43d4bf30d8be7ded60f381459c0
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2012-10-15 20:46:17 +0200

    Remove duplicate language code from test settings

--- froide/test_settings.py
+++ froide/test_settings.py
@@ -50,9 +50,6 @@ DATABASES = {
 TIME_ZONE = 'Europe/Berlin'
 USE_TZ = True
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'de'
 
 LOCALE_PATHS = (
     os.path.join(PROJECT_ROOT, "locale"),
