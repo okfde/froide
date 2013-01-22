7741ba0d43199bd801a2d8db09aead731a007589
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-01-22 17:03:34 +0100

    Change order of template loaders to prefer apps

--- froide/settings.py
+++ froide/settings.py
@@ -123,9 +123,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-    # 'django.template.loaders.eggs.Loader',
+    'django.template.loaders.filesystem.Loader',
 )
 
 MIDDLEWARE_CLASSES = [
