48b70cf62cdb516f1779b020b99a073e00c276ca
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-03-01 22:03:20 +0100

    Change theme settings
    Remove OKF logo because it's not https

--- docs/conf.py
+++ docs/conf.py
@@ -100,6 +100,8 @@ if os.path.exists(os.path.join(os.path.abspath('_themes'), _html_theme)):
     html_theme = _html_theme
     html_theme_options = {
         'logo_icon': '_static/froide.png',
+        'show_okfn_logo': False,
+        'show_version': True
     }
 
 # Theme options are theme-specific and customize the look and feel of a theme
