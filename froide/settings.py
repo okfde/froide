5879fa3e5ee5538cbe27c735f76b32283fbee393
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-03-01 03:29:30 +0100

    Find static files in app dirs first

--- froide/settings.py
+++ froide/settings.py
@@ -154,8 +154,8 @@ AUTH_PROFILE_MODULE = 'account.Profile'
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
