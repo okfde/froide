a26f22163c9e6a39ec17e6b8b58242fd45187568
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-04-23 21:51:04 +0200

    Fix order in followed events view

--- froide/account/views.py
+++ froide/account/views.py
@@ -85,7 +85,8 @@ def show(request, context=None, status=200):
         since = request.user.last_login - timedelta(days=14)
         events = FoiEvent.objects.filter(public=True,
                 request__in=followed_foirequest_ids,
-                timestamp__gte=since)[:10]
+                timestamp__gte=since).order_by(
+                    'request', 'timestamp')[:10]
     context.update({
         'own_requests': own_foirequests,
         'followed_requests': followed_requests,
