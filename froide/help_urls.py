72cf968219d431eeb6de2243e527557a4eebcfec
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2013-12-07 15:00:01 +0100

    Restructure help urls and views, add default template
    Can now handle user set language and fall back to
    default settings language

--- froide/help_urls.py
+++ froide/help_urls.py
@@ -1,29 +1,46 @@
-from django.conf.urls import patterns
 from django.conf import settings
+from django.conf.urls import patterns
+from django.shortcuts import render
+from django.http import Http404
+from django.template import TemplateDoesNotExist
 from django.utils.translation import ugettext as _
-from django.views.generic import TemplateView
-
-LG = settings.LANGUAGE_CODE
 
 
-def TV(template):
-    return TemplateView.as_view(template_name=template)
+def help_view(request, template=None, language=None):
+    language = language or request.LANGUAGE_CODE
+    template_name = 'help/{}/{}'.format(language, template)
+    try:
+        return render(request, template_name)
+    except TemplateDoesNotExist:
+        pass
+    if len(language) > 2:
+        return help_view(request, template=template, language=language[:2])
+    if not settings.LANGUAGE_CODE.startswith(language):
+        return help_view(request, template=template,
+                         language=settings.LANGUAGE_CODE)
+    raise Http404
+
 
 urlpatterns = patterns("",
-    (r'^$', TV("help/%s/index.html" % LG), {}, 'help-index'),
+    (r'^$', help_view, {'template': 'index.html'}, 'help-index'),
     # Translators: URL part of /help/
-    (r'^%s/$' % _('about'), TV("help/%s/about.html" % LG), {}, 'help-about'),
+    (r'^%s/$' % _('about'), help_view, {'template': 'about.html'},
+        'help-about'),
     # Translators: URL part of /help/
-    (r'^%s/$' % _('terms'), TV("help/%s/terms.html" % LG), {}, 'help-terms'),
+    (r'^%s/$' % _('terms'), help_view, {'template': 'terms.html'},
+        'help-terms'),
     # Translators: URL part of /help/
-    (r'^%s/$' % _('privacy'), TV("help/%s/privacy.html" % LG), {}, 'help-privacy'),
+    (r'^%s/$' % _('privacy'), help_view, {'template': 'privacy.html'},
+        'help-privacy'),
     # Translators: URL part of /help/
-    (r'^%s/$' % _('making-requests'), TV("help/%s/making-requests.html" % LG), {}, 'help-making_requests'),
+    (r'^%s/$' % _('making-requests'), help_view,
+        {'template': 'making-requests.html'}, 'help-making_requests'),
     # Translators: URL part of /help/
-    (r'^%s/$' % _('your-privacy'), TV("help/%s/your-privacy.html" % LG), {}, 'help-your_privacy'),
+    (r'^%s/$' % _('your-privacy'), help_view,
+        {'template': 'your-privacy.html'}, 'help-your_privacy'),
     # Translators: URL part of /help/
-    (r'^%s/$' % _('for-foi-officers'), TV("help/%s/foi-officers.html" % LG), {},
-        'help-foi_officers'),
-    (r'^%s/$' % _('donate'), TV("help/%s/donate.html" % LG), {},
+    (r'^%s/$' % _('for-foi-officers'), help_view,
+        {'template': 'foi-officers.html'}, 'help-foi_officers'),
+    (r'^%s/$' % _('donate'), help_view, {'template': 'donate.html'},
         'help-donate'),
 )
