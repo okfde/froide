9be5e1d4592c8ee2cdf04eb78bf0a7f6fa6c43cc
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2015-08-06 17:14:51 +0200

    Fix name generator for Python 3

--- froide/helper/name_generator.py
+++ froide/helper/name_generator.py
@@ -2833,8 +2833,8 @@ u'the_tomorrow_man_zarrko'
 
 def get_float_from_string(seed):
     ''' Probably bad way to get a float from secret key'''
-    max_sha_float = float(115792089237316195423570985008687907853269984665640564039457584007913129639935L)
-    h = hashlib.sha256(seed)
+    max_sha_float = float(115792089237316195423570985008687907853269984665640564039457584007913129639935)
+    h = hashlib.sha256(seed.encode('utf-8'))
     return int(h.hexdigest(), 16) / max_sha_float
 
 
