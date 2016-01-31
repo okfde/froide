bd2c1e94426ae35fb10ed8356f402b7d7a08f6bc
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-01-31 19:34:36 +0100

    Add X-Forward-For Middleware

---
+++ froide/helper/middleware.py
@@ -0,0 +1,5 @@
+class XForwardedForMiddleware():
+    def process_request(self, request):
+        if 'HTTP_X_FORWARDED_FOR' in request.META:
+            request.META['REMOTE_ADDR'] = request.META['HTTP_X_FORWARDED_FOR'].split(",")[0].strip()
+        return None
