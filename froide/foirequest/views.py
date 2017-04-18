b2eb16b824320e2c55e66b8e8d539170ee6e3da9
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-04-18 02:51:54 +0200

    Fix filter expression for Python 3

--- froide/foirequest/views.py
+++ froide/foirequest/views.py
@@ -229,16 +229,13 @@ def show(request, slug, template_name="foirequest/show.html",
         message.request = obj
         if message.not_publishable:
             obj.not_publishable_message = message
-        message.all_attachments = filter(
-            lambda x: x.belongs_to_id == message.id, all_attachments)
-        message.approved_attachments = filter(
-            lambda x: x.belongs_to_id == message.id and x.approved,
-            all_attachments
-        )
-        message.not_approved_attachments = filter(
-            lambda x: x.belongs_to_id == message.id and not x.approved,
-            all_attachments
-        )
+        message.all_attachments = [a for a in all_attachments
+                    if a.belongs_to_id == message.id]
+        message.approved_attachments = [a for a in all_attachments
+                    if a.belongs_to_id == message.id and a.approved]
+        message.not_approved_attachments = [a for a in all_attachments
+                    if a.belongs_to_id == message.id and not a.approved]
+
         for att in message.all_attachments:
             att.belongs_to = message
 
