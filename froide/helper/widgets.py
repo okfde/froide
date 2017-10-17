a2c5c3253c7be46b39503c8585276d3de5a5bc22
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-10-17 16:13:06 +0200

    Remove static call on widget media as is now automatic

--- froide/helper/widgets.py
+++ froide/helper/widgets.py
@@ -1,13 +1,11 @@
 from __future__ import unicode_literals
 
 from django import forms
-from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from django.forms.widgets import TextInput
 from django.conf import settings
 from django.utils import six
-from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.utils.translation import ugettext_lazy as _
 
 from taggit.utils import edit_string_for_tags
@@ -55,15 +53,15 @@ class TagAutocompleteTagIt(TextInput):
 
         # load js
         js = (
-            static('admin/js/vendor/jquery/jquery.min.js'),
-            static('%stagging_autocomplete_tagit.js' % js_base_url),
-            static(jqueryui_file),
-            static('%sjquery.tag-it.min.js' % js_base_url),
+            'admin/js/vendor/jquery/jquery.min.js',
+            '%stagging_autocomplete_tagit.js' % js_base_url,
+            jqueryui_file,
+            '%sjquery.tag-it.min.js' % js_base_url,
         )
 
         # custom css can also be overriden in settings
         css_list = getattr(settings, 'TAGGING_AUTOCOMPLETE_CSS',
-            [static('%scss/ui-autocomplete-tag-it.css' % js_base_url)])
+            ['%scss/ui-autocomplete-tag-it.css' % js_base_url])
         # check is a list, if is a string convert it to a list
         if type(css_list) != list and type(css_list) == str:
             css_list = [css_list]
