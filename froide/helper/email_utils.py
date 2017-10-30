81163787b34f080703c1b11ef0632dcd3a0a503b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-10-30 13:00:48 +0000

    Fix PEP8

--- froide/helper/email_utils.py
+++ froide/helper/email_utils.py
@@ -13,15 +13,14 @@ import time
 import base64
 
 try:
-    from email.header import Header, decode_header
+    from email.header import decode_header
     from email.parser import BytesParser as Parser
 except ImportError:
-    from email.Header import Header, decode_header
+    from email.Header import decode_header
     from email.Parser import Parser
 
 from email.utils import parseaddr, formataddr, parsedate_tz, getaddresses
 import imaplib
-import re
 
 from django.utils.six import BytesIO, text_type as str, binary_type as bytes
 
