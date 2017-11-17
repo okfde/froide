1b3bf6e8c1169c5495d9f05b1f68076c860ca724
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-11-17 17:40:09 +0100

    Remove storage backend module

--- froide/helper/storage_utils.py
+++
@@ -1,7 +0,0 @@
-from storages.backends.s3boto import S3BotoStorage
-
-
-class ForceAuthS3BotoStorage(S3BotoStorage):
-    def __init__(self, *args, **kwargs):
-        kwargs['querystring_auth'] = True
-        super(ForceAuthS3BotoStorage, self).__init__(*args, **kwargs)
