290aa8c3cb8c8520100f0fc861a62934ff00fe2e
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-12-19 10:05:09 +0600

    Add admin filter on messages for delivery status

--- froide/foirequest/admin.py
+++ froide/foirequest/admin.py
@@ -171,7 +171,7 @@ class DeliveryStatusInline(admin.TabularInline):
 class FoiMessageAdmin(admin.ModelAdmin):
     save_on_top = True
     list_display = ('subject', 'timestamp', 'sender_email', 'recipient_email',)
-    list_filter = ('is_postal', 'is_response', 'sent', 'status',)
+    list_filter = ('is_postal', 'is_response', 'sent', 'status', 'deliverystatus__status')
     search_fields = ['subject', 'sender_email', 'recipient_email']
     ordering = ('-timestamp',)
     date_hierarchy = 'timestamp'
