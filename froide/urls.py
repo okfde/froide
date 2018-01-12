b6ef6df18a938f324327c3d8329846922787e25b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-01-12 13:31:50 +0700

    Add debug toolbar to url conf
    Only if present and DEBUG = True

--- froide/urls.py
+++ froide/urls.py
@@ -167,6 +167,15 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if settings.DEBUG:
+    try:
+        import debug_toolbar
+        urlpatterns = [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
+    except ImportError:
+        pass
+
 # Catch all Jurisdiction patterns
 urlpatterns += [
     url(r'^(?P<slug>[\w-]+)/', include('froide.publicbody.jurisdiction_urls'))
