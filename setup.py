8a62d2c93106ec77fbd2829782e564da24a0c1db
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-02-02 15:13:49 +0100

    Change Python versions in setup.py

--- setup.py
+++ setup.py
@@ -60,9 +60,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP'
     ]
 )
