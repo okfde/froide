2bf5603291932265327f7d5cb9fac4a44b2e047b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-03-12 18:12:44 +0100

    Extract GeoRegion admin settings to Mixin class
    Avoids loading OpenLayers from cloudflare
    when just using the mixin

--- froide/georegion/admin.py
+++ froide/georegion/admin.py
@@ -3,11 +3,15 @@ from django.contrib.gis import admin
 from .models import GeoRegion
 
 
-class GeoRegionAdmin(admin.GeoModelAdmin):
+class GeoRegionMixin(object):
     search_fields = ['name', 'region_identifier']
     list_display = ('name', 'kind', 'region_identifier')
     list_filter = ('kind',)
     raw_id_fields = ('part_of',)
+
+
+class GeoRegionAdmin(GeoRegionMixin, admin.GeoModelAdmin):
+    pass
 
 
 admin.site.register(GeoRegion, GeoRegionAdmin)
