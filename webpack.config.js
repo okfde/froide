23449bcaaaa35dedc90898a9c7704649136825c5
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-03-29 17:36:44 +0200

    Add froide_static webpack alias

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,9 @@ const config = {
     modules: ['node_modules', 'froide/static'],
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      'vue$': 'vue/dist/vue.esm.js',
+      'froide': path.resolve('.'),
+      'froide_static': path.resolve('.', 'froide', 'static')
     }
   },
   module: {
