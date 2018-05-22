a54ec9391f51b2c4b5babe48c61592446245ab25
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-05-22 18:52:34 +0200

    Fix bug in block_helper tag
    render_block needs to parse and render rest of document

--- froide/helper/templatetags/block_helper.py
+++ froide/helper/templatetags/block_helper.py
@@ -31,10 +31,25 @@ class AddToBlockNode(template.Node):
         return ''
 
 
-@register.simple_tag(takes_context=True)
-def render_block(context, block_name):
-    if VAR_NAME not in context:
-        return ''
-    if block_name not in context[VAR_NAME]:
-        return ''
-    return mark_safe('\n'.join(context[VAR_NAME][block_name].keys()))
+@register.tag(name="render_block")
+def do_render_block(parser, token):
+    name = token.split_contents()[1][1:-1]
+    nodelist = parser.parse()
+    return RenderBlockNode(name, nodelist)
+
+
+class RenderBlockNode(template.Node):
+    def __init__(self, block_name, nodelist):
+        self.block_name = block_name
+        self.nodelist = nodelist
+
+    def render(self, context):
+        output = self.nodelist.render(context).strip()
+        return self.get_block_contents(context) + output
+
+    def get_block_contents(self, context):
+        if VAR_NAME not in context:
+            return ''
+        if self.block_name not in context[VAR_NAME]:
+            return ''
+        return mark_safe('\n'.join(context[VAR_NAME][self.block_name].keys()))
