106c8085c2dbe700a999002a70c213308057682e
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-05-31 16:30:38 +0200

    Use OrderedDict for block helper

--- froide/helper/templatetags/block_helper.py
+++ froide/helper/templatetags/block_helper.py
@@ -28,9 +28,7 @@ class AddToBlockNode(template.Node):
     def render(self, context):
         output = self.nodelist.render(context).strip()
         if VAR_NAME not in context:
-            context[VAR_NAME] = {}
-        if self.block_name not in context[VAR_NAME]:
-            context[VAR_NAME][self.block_name] = {}
+            context[VAR_NAME] = get_default_dict()
         context[VAR_NAME][self.block_name][output] = None
         return ''
 
