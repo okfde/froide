f11ac1a86fe89f2fab2caf8052b054169fd761e3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-06-04 12:49:28 +0200

    Remove full text checkbox from live test
    Only available when user is logged in

--- froide/tests/live/test_request.py
+++ froide/tests/live/test_request.py
@@ -236,8 +236,6 @@ class TestMakingRequest(LiveTestMixin, StaticLiveServerTestCase):
                 lambda driver: driver.find_element_by_name('body').is_displayed()
             )
             self.selenium.find_element_by_name('subject').send_keys(req_title)
-            self.scrollTo('full_text_checkbox')
-            self.selenium.find_element_by_name('full_text_checkbox').click()
             self.scrollTo('id_body')
             self.selenium.find_element_by_name('body').clear()
             body_text = 'Documents describing & something...'
@@ -259,8 +257,8 @@ class TestMakingRequest(LiveTestMixin, StaticLiveServerTestCase):
         req = FoiRequest.objects.filter(user=self.user).order_by('-id')[0]
         self.assertIn(req.get_absolute_url(), self.selenium.current_url)
         self.assertEqual(req.title, req_title)
-        self.assertEqual(req.description, body_text)
-        self.assertTrue(req.messages[0].plaintext.startswith(body_text))
+        self.assertIn(req.description, body_text)
+        self.assertIn(body_text, req.messages[0].plaintext)
         self.assertEqual(req.public, True)
         self.assertEqual(req.public_body, self.pb)
         self.assertEqual(req.status, 'awaiting_response')
