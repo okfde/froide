31a7ebe259e0c794dd8fed6afdde2ec2ed889a31
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-06-22 19:01:13 +0200

    Fix hosted viewer origin check of pdf.js

--- froide/document/static/document/viewer/web/viewer.js
+++ froide/document/static/document/viewer/web/viewer.js
@@ -1361,7 +1361,7 @@ var PDFViewerApplication = {
 };
 var validateFileURL = void 0;
 {
-  var HOSTED_VIEWER_ORIGINS = ['null', 'http://mozilla.github.io', 'https://mozilla.github.io'];
+  var HOSTED_VIEWER_ORIGINS = ['null', 'https://static.frag-den-staat.de'];
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
       return;
