25bd6850e951c1a3496b766fd79b059780b80545
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-09-06 13:44:05 +0200

    Remove Python 2.7 reference

--- setup.py
+++ setup.py
@@ -66,7 +66,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP'
