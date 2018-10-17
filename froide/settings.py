db4ba46351972132488e77bf8f64fb44280afd8d
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-10-17 20:13:12 +0200

    Add queue for search indexing tasks

--- froide/settings.py
+++ froide/settings.py
@@ -358,7 +358,8 @@ class Base(Configuration):
     CELERY_TASK_ROUTES = {
         'froide.foirequest.tasks.fetch_mail': {"queue": "emailfetch"},
         'froide.foirequest.tasks.process_mail': {"queue": "email"},
-        'djcelery_email_send_multiple': {"queue": "email"},
+        'djcelery_email_send_multiple': {"queue": "emailsend"},
+        'froide.helper.tasks.*': {"queue": "searchindex"},
     }
     CELERY_TIMEZONE = 'UTC'
     # We need to serialize email data as binary
