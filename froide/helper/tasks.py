473c902e46d76cba1e7600235f8d0cccbd49d46b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-10-17 20:13:00 +0200

    Add expiry and logging to search  indexing tasks

--- froide/helper/tasks.py
+++ froide/helper/tasks.py
@@ -1,25 +1,30 @@
+import logging
+
 from django_elasticsearch_dsl.registries import registry
 
 from froide.celery import app as celery_app
 
+logger = logging.getLogger(__name__)
 
-@celery_app.task
+
+@celery_app.task(expires=60 * 60)
 def search_instance_save(instance):
     try:
+        print('indexing in celery')
         registry.update(instance)
         registry.update_related(instance)
-    except Exception:
-        pass
+    except Exception as e:
+        logger.exception(e)
 
 
-@celery_app.task
+@celery_app.task(expires=60 * 60)
 def search_instance_pre_delete(instance):
     try:
         registry.delete_related(instance)
-    except Exception:
-        pass
+    except Exception as e:
+        logger.exception(e)
 
 
-@celery_app.task
+@celery_app.task(expires=60 * 60)
 def search_instance_delete(instance):
     registry.delete(instance, raise_on_error=False)
