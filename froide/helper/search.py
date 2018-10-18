58ce8f83cf67f75a9e5ed1d0b937cd446bd79737
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-10-18 13:25:44 +0200

    Revert "Remove from index on save when no longer public"
    This reverts commit 21b4dd7ffca72060f52b7bcc0926db1ed587b5c9.

--- froide/helper/search.py
+++ froide/helper/search.py
@@ -313,10 +313,7 @@ class CelerySignalProcessor(RealTimeSignalProcessor):
         Given an individual model instance, update the object in the index.
         Update the related objects either.
         """
-        if instance.in_public_search_index():
-            search_instance_delete.delay(instance)
-        else:
-            search_instance_save.delay(instance)
+        search_instance_save.delay(instance)
 
     def handle_pre_delete(self, sender, instance, **kwargs):
         """Handle removing of instance object from related models instance.
