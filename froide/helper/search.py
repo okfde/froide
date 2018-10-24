ff66806d27a0f49e804eb483d4df31c93b1d43e5
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-10-24 11:39:38 +0200

    Check pagination offset against ES max

--- froide/helper/search.py
+++ froide/helper/search.py
@@ -283,12 +283,18 @@ class ElasticsearchPaginator(Paginator):
     Paginator that prevents two queries to ES (for count and objects)
     as ES gives count with objects
     """
+    MAX_ES_OFFSET = 10000
 
     def page(self, number):
         """
         Returns a Page object for the given 1-based page number.
         """
         bottom = (number - 1) * self.per_page
+        if bottom >= self.MAX_ES_OFFSET:
+            # Only validate if bigger than offset
+            number = self.validate_number(number)
+            bottom = (number - 1) * self.per_page
+
         top = bottom + self.per_page
         self.object_list = self.object_list[bottom:top]
 
