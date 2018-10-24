5eeabdb2ae335c15d546935351b5edffb3416507
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-10-24 16:29:04 +0200

    Deactivate user when too many bounces occur

--- froide/bounce/utils.py
+++ froide/bounce/utils.py
@@ -4,6 +4,7 @@ https://en.wikipedia.org/wiki/Variable_envelope_return_path
 
 """
 from contextlib import closing
+from datetime import datetime, timedelta
 from io import BytesIO
 
 from django.conf import settings
@@ -19,6 +20,12 @@ BOUNCE_FORMAT = settings.FROIDE_CONFIG['bounce_format']
 SIGN_SEP = ':'
 SEP_REPL = '+'
 MAX_BOUNCE_AGE = settings.FROIDE_CONFIG['bounce_max_age']
+
+HARD_BOUNCE_COUNT = 3
+HARD_BOUNCE_PERIOD = timedelta(seconds=3 * 7 * 24 * 60 * 60)  # 3 weeks
+
+SOFT_BOUNCE_COUNT = 5
+SOFT_BOUNCE_PERIOD = timedelta(seconds=5 * 7 * 24 * 60 * 60)  # 5 weeks
 
 
 def make_bounce_address(email):
@@ -84,9 +91,39 @@ def add_bounce_mail(email):
     ])
     for recipient, status in recipient_list:
         if status:
-            Bounce.objects.update_bounce(recipient, email.bounce_info)
+            bounce = Bounce.objects.update_bounce(recipient, email.bounce_info)
+            check_user_deactivation(bounce)
         else:
             mail_managers(
                 'Bad bounce address found',
                 '%s: %s (%s)' % (email.subject, recipient, status)
             )
+
+
+def get_bounce_stats(bounces, bounce_type='hard'):
+    filtered_bounces = [
+        datetime.strptime(b['timestamp'][:19], '%Y-%m-%dT%H:%M:%S')
+        for b in bounces if b['bounce_type'] == bounce_type
+    ]
+    min_date, max_date = min(filtered_bounces), max(filtered_bounces)
+    diff = max_date - min_date
+    return len(filtered_bounces), diff
+
+
+def check_user_deactivation(bounce):
+    """
+    Decide if current bounce state warrants deactivation
+    """
+    if not bounce.user:
+        return
+
+    count, diff = get_bounce_stats(bounce.bounces, bounce_type='hard')
+    if count >= HARD_BOUNCE_COUNT and diff >= HARD_BOUNCE_PERIOD:
+        bounce.user.deactivate()
+        return True
+
+    count, diff = get_bounce_stats(bounce.bounces, bounce_type='soft')
+    if count >= SOFT_BOUNCE_COUNT and diff >= SOFT_BOUNCE_PERIOD:
+        bounce.user.deactivate()
+        return True
+    return False
