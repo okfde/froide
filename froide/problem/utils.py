a0dff46164b845b3868416d39f7e3617e7f4887f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-11-07 16:38:47 +0100

    Fix bad method call

--- froide/problem/utils.py
+++ froide/problem/utils.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 def inform_managers(report):
     mail_managers(
         _('New problem: {label} [#{reqid}]').format(
-            label=report.get_display_kind(),
+            label=report.get_kind_display(),
             reqid=report.message.request_id
         ),
         '{}\n{}'.format(
