c7987222e16d055719af265d275f112bb4029108
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-11-07 19:55:26 +0100

    Send problem resolution notice to user

--- froide/problem/admin.py
+++ froide/problem/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 from .models import ProblemReport
+from .utils import inform_user_problem_resolved
 
 
 class ProblemReportAdmin(admin.ModelAdmin):
@@ -16,6 +19,17 @@ class ProblemReportAdmin(admin.ModelAdmin):
         qs = super().get_queryset(request)
         qs = qs.select_related('message')
         return qs
+
+    def save_model(self, request, obj, form, change):
+        if 'resolved' in form.changed_data and obj.resolved:
+            if not obj.resolution_timestamp:
+                obj.resolution_timestamp = timezone.now()
+            sent = inform_user_problem_resolved(obj)
+            if sent:
+                self.message_user(
+                    request, _('User will be notified of resolution')
+                )
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(ProblemReport, ProblemReportAdmin)
--- froide/problem/utils.py
+++ froide/problem/utils.py
@@ -1,4 +1,6 @@
 from django.core.mail import mail_managers
+from django.conf import settings
+from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -13,3 +15,23 @@ def inform_managers(report):
             report.get_absolute_domain_url()
         )
     )
+
+
+def inform_user_problem_resolved(report):
+    if report.auto_submitted or not report.user:
+        return False
+
+    foirequest = report.message.request
+    subject = _('Problem resolved on your request')
+    body = render_to_string("problem/email_problem_resolved.txt", {
+        "user": report.user,
+        "title": foirequest.title,
+        "report": report,
+        "url": report.user.get_autologin_url(
+            report.message.get_absolute_short_url()
+        ),
+        "site_name": settings.SITE_NAME
+    })
+
+    report.user.send_mail(subject, body)
+    return True
