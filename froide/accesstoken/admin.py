41f254bd53ca6998725c959d9abe71975cffc92f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-11-14 14:33:27 +0100

    Add search, filter to access tokens

--- froide/accesstoken/admin.py
+++ froide/accesstoken/admin.py
@@ -5,6 +5,8 @@ from .models import AccessToken
 
 class AccessTokenAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)
+    list_filter = ('purpose',)
+    search_fields = ('user__email',)
 
 
 admin.site.register(AccessToken, AccessTokenAdmin)
