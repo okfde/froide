93901ccd8be197627ee18a4a722a64efd51ba11a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-11-15 09:48:50 +0100

    Use comma-separated regions for csv export

--- froide/publicbody/models.py
+++ froide/publicbody/models.py
@@ -465,7 +465,8 @@ class PublicBody(models.Model):
             "jurisdiction__slug",
             ("categories", lambda x: edit_string_for_tags(x.categories.all())),
             "other_names", "website_dump", "description",
-            "request_note", "parent__name", 'region_id',
+            "request_note", "parent__name",
+            ('regions', lambda obj: ','.join(str(x.id) for x in obj.regions.all()))
         )
 
         return export_csv(queryset, fields)
