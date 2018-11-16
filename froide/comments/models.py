5cfee3dcd9b42997c7b2bf0241e7d0a32bd7d191
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-11-16 16:22:34 +0100

    Add migrations for comment model

--- froide/comments/models.py
+++ froide/comments/models.py
@@ -3,6 +3,3 @@ from django_comments.abstracts import CommentAbstractModel
 
 class FroideComment(CommentAbstractModel):
     ip_address = None
-
-    class Meta(CommentAbstractModel.Meta):
-        db_table = "django_comments"
