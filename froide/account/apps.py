e1ddb2ec841502f0b4bc4d5874822c18957cce07
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-12-05 13:55:26 +0100

    Listen for bounced email signal in account

--- froide/account/apps.py
+++ froide/account/apps.py
@@ -5,3 +5,16 @@ from django.utils.translation import ugettext_lazy as _
 class AccountConfig(AppConfig):
     name = 'froide.account'
     verbose_name = _("Account")
+
+    def ready(self):
+        from froide.bounce.signals import user_email_bounced
+
+        user_email_bounced.connect(deactivate_user_after_bounce)
+
+
+def deactivate_user_after_bounce(bounce, should_deactivate=False, **kwargs):
+    if not should_deactivate:
+        return
+    if not bounce.user:
+        return
+    bounce.user.deactivate()
