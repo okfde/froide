2d2a8b4ff37ce0a55629f29c0ab0de7082809f7f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2018-12-05 22:00:56 +0100

    Move ajax form handling into main JS file

--- frontend/javascript/request.ts
+++ frontend/javascript/request.ts
@@ -5,14 +5,6 @@ import {Tab, Tooltip} from 'bootstrap.native'
 
 interface HTMLTabElement extends HTMLElement { Tab: Tab | null; }
 
-const serializeForm = (form: HTMLFormElement) => {
-  const enabled = (<HTMLInputElement[]> Array.from(form.elements)).filter(node => !node.disabled)
-  const pairs = enabled.map(node => {
-    const encoded = [node.name, node.value].map(encodeURIComponent)
-    return encoded.join('=')
-  })
-  return pairs.join('&')
-}
 
 const addText = (dataset: DOMStringMap) => {
   if (!dataset.addtextfield) {
@@ -170,39 +162,6 @@ const runOnPage = () => {
     }
   }
 
-  const ajaxForms = document.querySelectorAll('form.ajaxified')
-  Array.from(ajaxForms).forEach(form => {
-    form.addEventListener('submit', function (e) {
-      e.preventDefault()
-
-      const method = form.getAttribute('method') || 'post'
-      const url = form.getAttribute('action') || ''
-      const data = serializeForm(<HTMLFormElement> form)
-
-      var request = new XMLHttpRequest();
-      request.open(method, url, true);
-      request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-      request.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
-      request.onload = function() {
-        if (request.status === 302) {
-          const loc = request.getResponseHeader('Location')
-          window.location.href = loc || ''
-        }
-        const data = request.responseText
-        if (data.length > 0) {
-          const parent = form.closest('.ajax-parent')
-          if (parent) {
-            parent.outerHTML = data;
-          }
-        }
-      }
-      request.send(data);
-
-      Array.from(form.querySelectorAll('button, input')).forEach(el => {
-        el.setAttribute('disabled', '')
-      })
-    })
-  })
 
   interface MessageOffsetCache { top: number, height: number, id: string }
 
---
+++ frontend/javascript/snippets/form-ajax.ts
@@ -0,0 +1,52 @@
+const serializeForm = (form: HTMLFormElement) => {
+    const enabled = (<HTMLInputElement[]> Array.from(form.elements)).filter(node => !node.disabled)
+    const pairs = enabled.map(node => {
+      const encoded = [node.name, node.value].map(encodeURIComponent)
+      return encoded.join('=')
+    })
+    return pairs.join('&')
+  }
+
+const submitFormsAjax = () => {
+  const ajaxForms = document.querySelectorAll('form.ajaxified')
+  Array.from(ajaxForms).forEach(form => {
+    form.addEventListener('submit', function (e) {
+      e.preventDefault()
+
+      const method = form.getAttribute('method') || 'post'
+      const url = form.getAttribute('action') || ''
+      const data = serializeForm(<HTMLFormElement> form)
+
+      var request = new XMLHttpRequest();
+      request.open(method, url, true);
+      request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      request.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+      request.onload = function() {
+        const data = request.responseText
+        if (data.length > 0) {
+          if (data[0] == '/') {
+            // starts with URL, redirect
+            window.location.href = data
+            return
+          }
+          const parent = form.closest('.ajax-parent')
+          if (parent) {
+            parent.outerHTML = data;
+          }
+        }
+      }
+      request.send(data);
+
+      Array.from(form.querySelectorAll('button, input')).forEach(el => {
+        el.setAttribute('disabled', '')
+      })
+    })
+  })
+}
+
+submitFormsAjax()
+
+export default {
+  serializeForm,
+  submitFormsAjax
+}
