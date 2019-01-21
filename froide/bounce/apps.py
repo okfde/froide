b1b1392d2f268a5c74fd21c826a3ea6387567cab
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-21 14:55:53 +0100

    Add user data export for bounce handling

--- froide/bounce/apps.py
+++ froide/bounce/apps.py
@@ -1,3 +1,5 @@
+import json
+
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,8 +10,10 @@ class BounceConfig(AppConfig):
 
     def ready(self):
         from froide.account import account_canceled
+        from froide.account.export import registry
 
         account_canceled.connect(cancel_user)
+        registry.register(export_user_data)
 
 
 def cancel_user(sender, user=None, **kwargs):
@@ -18,3 +22,21 @@ def cancel_user(sender, user=None, **kwargs):
     if user is None:
         return
     Bounce.objects.filter(user=user).delete()
+
+
+def export_user_data(user):
+    from .models import Bounce
+
+    bounces = Bounce.objects.filter(user=user)
+    if not bounces:
+        return
+    yield ('bounces.json', json.dumps([
+        {
+            'last_update': (
+                b.last_update.isoformat() if b.last_update else None
+            ),
+            'bounces': b.bounces,
+            'email': b.email,
+        }
+        for b in bounces]).encode('utf-8')
+    )
