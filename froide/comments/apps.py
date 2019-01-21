9de0a05d28c83742224c0e708e80b8add198a8a8
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-01-21 14:55:59 +0100

    Add user data export for comments

--- froide/comments/apps.py
+++ froide/comments/apps.py
@@ -1,3 +1,5 @@
+import json
+
 from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
@@ -8,8 +10,10 @@ class CommentConfig(AppConfig):
 
     def ready(self):
         from froide.account import account_canceled
+        from froide.account.export import registry
 
         account_canceled.connect(cancel_user)
+        registry.register(export_user_data)
 
 
 def cancel_user(sender, user=None, **kwargs):
@@ -21,4 +25,24 @@ def cancel_user(sender, user=None, **kwargs):
         user_name='',
         user_email='',
         user_url=''
+    )
+
+
+def export_user_data(user):
+    from .models import FroideComment
+
+    comments = FroideComment.objects.filter(user=user)
+    if not comments:
+        return
+    yield ('comments.json', json.dumps([
+        {
+            'submit_date': (
+                c.submit_date.isoformat() if c.submit_date else None
+            ),
+            'comment': c.comment,
+            'is_public': c.is_public,
+            'is_removed': c.is_removed,
+            'url': c.get_absolute_url(),
+        }
+        for c in comments]).encode('utf-8')
     )
