a4a2230d7ae4f89c25d3ad0f13d30ba4330e3031
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-02-10 19:49:08 +0100

    Improve account tags, copy of make same request

--- froide/account/templatetags/account_tags.py
+++ froide/account/templatetags/account_tags.py
@@ -6,15 +6,11 @@ from ..menu import menu_registry
 register = template.Library()
 
 
-@register.simple_tag(takes_context=True)
-def get_new_user_form(context, var_name):
-    form = NewUserForm()
-    context[var_name] = form
-    return ""
+@register.simple_tag
+def get_new_user_form():
+    return NewUserForm()
 
 
 @register.simple_tag(takes_context=True)
-def get_menu_items(context, var_name):
-    menu_items = menu_registry.get_menu_items(context['request'])
-    context[var_name] = menu_items
-    return ""
+def get_menu_items(context):
+    return menu_registry.get_menu_items(context['request'])
