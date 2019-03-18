09cb0ff91d628ac48938fd9320256e5aceb4b16d
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-03-18 22:22:57 +0100

    Fix autocomplete widget rendering after upgrade

--- froide/helper/widgets.py
+++ froide/helper/widgets.py
@@ -70,7 +70,7 @@ class TagAutocompleteWidget(forms.TextInput):
     def render(self, name, value, attrs=None, renderer=None):
         """ Render HTML code """
         if value is not None and not isinstance(value, str):
-            value_list = [o.tag for o in value.select_related('tag')]
+            value_list = [o.tag for o in value]
             value = edit_string_for_tags(value_list)
 
         options = [
