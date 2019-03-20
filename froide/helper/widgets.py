e03641512c51029e162bbfd588b1ac1fc14008ee
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-03-20 14:46:44 +0100

    Refactor tag autocomplete widget

--- froide/helper/widgets.py
+++ froide/helper/widgets.py
@@ -1,11 +1,7 @@
 from django import forms
-from django.utils.safestring import mark_safe
-
 from django.conf import settings
-from django.utils.html import escape
-from django.utils.translation import ugettext_lazy as _
 
-from taggit.utils import edit_string_for_tags, parse_tags
+from taggit.forms import TagWidget
 
 
 class BootstrapChoiceMixin(object):
@@ -42,7 +38,9 @@ class PriceInput(forms.TextInput):
         return ctx
 
 
-class TagAutocompleteWidget(forms.TextInput):
+class TagAutocompleteWidget(TagWidget):
+    template_name = 'helper/forms/widgets/tag_autocomplete.html'
+
     class Media:
 
         js = (
@@ -58,46 +56,10 @@ class TagAutocompleteWidget(forms.TextInput):
 
     def __init__(self, *args, **kwargs):
         self.autocomplete_url = kwargs.pop('autocomplete_url', None)
-        super(TagAutocompleteWidget, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
-    def value_from_datadict(self, data, files, name):
-        """ Force comma separation of tags by adding trailing comma """
-        val = data.get(name, None)
-        if val is None:
-            return ''
-        return val + ','
-
-    def render(self, name, value, attrs=None, renderer=None):
-        """ Render HTML code """
-        if value is not None and not isinstance(value, str):
-            value = edit_string_for_tags(value)
-
-        options = [
-            '<option value="{value}" selected>{value}</option>'.format(
-                value=escape(str(o))) for o in parse_tags(value)]
-        options = '\n'.join(options)
-
-        context = {
-            'data-additemtext': _('Press Enter to add <b>${value}</b>'),
-            'data-uniqueitemtext': _('This tag is already set.'),
-            'data-loading': _('Searching…'),
-            'data-noresults': _('No results'),
-            'data-nochoices': _('No results'),
-            'data-fetchurl': self.autocomplete_url
-        }
-        context = {k: escape(v) for k, v in context.items()}
-        context = ['%s="%s"' % (k, v) for k, v in context.items()]
-
-        html = super(TagAutocompleteWidget, self).render(
-            name, value, attrs, renderer=renderer
-        )
-        html = """<div style="display: none">{input}</div>
-        <select class="tagautocomplete"
-        {dataitems}
-        id="{objectid}_select" multiple>{options}</select>""".format(
-                input=html,
-                objectid=attrs['id'],
-                options=options,
-                dataitems=' '.join(context)
-        )
-        return mark_safe(html)
+    def get_context(self, name, value, attrs):
+        ctx = super().get_context(name, value, attrs)
+        ctx['autocomplete_url'] = self.autocomplete_url
+        ctx['tags'] = [v.tag.name for v in value]
+        return ctx
