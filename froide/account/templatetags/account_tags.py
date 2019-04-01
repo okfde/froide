119bddf211da63233290173fc0158391042a976d
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-04-01 13:36:37 +0200

    Allow making addresses in account forms required

--- froide/account/templatetags/account_tags.py
+++ froide/account/templatetags/account_tags.py
@@ -1,14 +1,23 @@
 from django import template
 
-from ..forms import NewUserForm
+from ..forms import NewUserForm, AddressForm
 from ..menu import menu_registry
 
 register = template.Library()
 
 
-@register.simple_tag
-def get_new_user_form():
-    return NewUserForm()
+@register.simple_tag(takes_context=True)
+def get_user_form(context, address_required=False):
+    request = context['request']
+    if request.user.is_authenticated:
+        return AddressForm(
+            initial={
+                'address': request.user.address
+            },
+            address_required=address_required
+        )
+    else:
+        return NewUserForm(address_required=address_required)
 
 
 @register.simple_tag(takes_context=True)
