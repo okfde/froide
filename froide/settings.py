bf3fdccfe037318579eec485c4ae291de37d6522
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-07-17 18:51:01 +0200

    Move heavy tasks into own queues

--- froide/settings.py
+++ froide/settings.py
@@ -396,6 +396,10 @@ class Base(Configuration):
         'froide.foirequest.tasks.process_mail': {"queue": "email"},
         'djcelery_email_send_multiple': {"queue": "emailsend"},
         'froide.helper.tasks.*': {"queue": "searchindex"},
+        'froide.foirequest.tasks.redact_attachment_task': {"queue": "redact"},
+        'froide.foirequest.tasks.ocr_pdf_task': {"queue": "ocr"},
+        'froide.foirequest.tasks.convert_images_to_pdf_task': {"queue": "convert"},
+        'froide.foirequest.tasks.convert_attachment_task': {"queue": "convert_office"},
     }
     CELERY_TIMEZONE = 'UTC'
     # We need to serialize email data as binary
