4895a3abdb484c458193d009171dba6ffb31dfb3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-08-15 16:19:47 +0200

    Add page API endpoint

--- froide/urls.py
+++ froide/urls.py
@@ -20,6 +20,7 @@ from froide.foirequestfollower.api_views import FoiRequestFollowerViewSet
 from froide.campaign.api_views import CampaignViewSet
 from froide.upload.api_views import UploadViewSet
 from froide.document.api_views import DocumentViewSet
+from filingcabinet.api_views import PageViewSet
 
 from froide.publicbody.views import (PublicBodySitemap, FoiLawSitemap,
                                      JurisdictionSitemap, show_publicbody,
@@ -55,6 +56,7 @@ api_router.register(r'following', FoiRequestFollowerViewSet, basename='following
 api_router.register(r'campaign', CampaignViewSet, basename='campaign')
 api_router.register(r'upload', UploadViewSet, basename='upload')
 api_router.register(r'document', DocumentViewSet, basename='document')
+api_router.register(r'page', PageViewSet, basename='page')
 
 
 class StaticViewSitemap(Sitemap):
