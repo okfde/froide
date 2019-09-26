c157ddeae7131e4141bca43857730103617b42c4
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-09-26 17:21:28 +0200

    Make guid required in upload API

--- froide/upload/serializers.py
+++ froide/upload/serializers.py
@@ -7,3 +7,7 @@ class UploadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Upload
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['guid'].required = True
