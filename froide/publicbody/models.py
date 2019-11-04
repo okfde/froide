58278d739ff9b7c9b6de673163a4da89cd7a5c3b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-11-04 16:56:28 +0100

    Export parent_id rather than parent name

--- froide/publicbody/models.py
+++ froide/publicbody/models.py
@@ -471,7 +471,7 @@ class PublicBody(models.Model):
             "jurisdiction__slug",
             ("categories", lambda x: edit_string_for_tags(x.categories.all())),
             "other_names", "website_dump", "description",
-            "request_note", "parent__name",
+            "request_note", "parent__id",
             ('regions', lambda obj: ','.join(str(x.id) for x in obj.regions.all()))
         )
 
