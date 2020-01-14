9e12119a9819ec84515e516f91d51c7d55a8275d
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-01-14 20:41:55 +0100

    Use new @okfde/filingcabinet dependency

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ const ENTRY = {
   docupload: ['./frontend/javascript/docupload.js'],
   geomatch: ['./frontend/javascript/geomatch.js'],
   messageredaction: ['./frontend/javascript/messageredaction.js'],
-  filingcabinet: 'filingcabinet/frontend/javascript/filingcabinet.js',
+  filingcabinet: '@okfde/filingcabinet/frontend/javascript/filingcabinet.js',
   publicbodyupload: ['./frontend/javascript/publicbodyupload.js']
 }
 
