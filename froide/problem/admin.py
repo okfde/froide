8b3bde7eb935c002ebe1a5e311f2c0a46734e66a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2020-07-14 14:02:53 +0200

    Fix problem admin raw_id_field on moderator

--- froide/problem/admin.py
+++ froide/problem/admin.py
@@ -10,7 +10,7 @@ from .utils import inform_user_problem_resolved
 
 class ProblemReportAdmin(admin.ModelAdmin):
     date_hierarchy = 'timestamp'
-    raw_id_fields = ('message', 'user',)
+    raw_id_fields = ('message', 'user', 'moderator')
     list_filter = ('auto_submitted', 'resolved', 'kind')
     list_display = (
         'kind', 'timestamp', 'admin_link_message',
