31648c50ce4df69bf40b2de0e739f5590fcdbd4b
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2020-08-19 16:10:23 +0200

    timeline WIP

--- frontend/javascript/alpha/Timeline.ts
+++ frontend/javascript/alpha/Timeline.ts
@@ -1,8 +1,37 @@
+import Message from './Message'
+
 export default class Timeline {
   root: HTMLElement
+  corrRoot: HTMLElement
+  messages: Message[]
   
-  constructor (element: HTMLElement) {
-    this.root = element
+  constructor (
+    correspondContainer: HTMLElement,
+    timelineContainer: HTMLElement,
+    messages: Message[]
+    ) {
+    this.root = timelineContainer
+    this.corrRoot = correspondContainer
+    this.messages = messages
+
+    this.setupScrollListener()
+  }
+
+  setupScrollListener () {
+    let timeout: number
+    window.addEventListener('scroll', () => {
+      // If there's a timer, cancel it
+      if (timeout) {
+        window.cancelAnimationFrame(timeout)
+      }
+
+      // Setup the new requestAnimationFrame()
+      timeout = window.requestAnimationFrame(this.update.bind(this));
+    }, false)
+  }
+
+  update () {
+    console.log('debounced', this);
   }
 
 }
--- frontend/javascript/alpha/request-alpha.ts
+++ frontend/javascript/alpha/request-alpha.ts
@@ -2,6 +2,37 @@ import Message from './Message'
 import Timeline from './Timeline'
 
 
+const init = () => {
+  // init message containers
+  const messages: Message[] = initMessageContainers()
+  if (!messages.length) return
+
+  // when all messages initialized:
+  // scroll to comment if query parameters given (e.g. ?msg=76058&c=10856)
+  // find message and comment containers with id that equal query params
+  const urlParams = new URLSearchParams(window.location.search);
+  const msgParam = urlParams.get('msg')
+  const commentParam = urlParams.get('c')
+  const scrollToMsgId = msgParam ? `nachricht-${msgParam}` : null
+  const scrollToCommentId = commentParam ? `comment-${commentParam}` : null
+  if (scrollToMsgId && scrollToCommentId) {
+    const msg = messages.find(m => m.id === scrollToMsgId)
+    if (msg) {
+      msg.scrollToComment(scrollToCommentId)
+    }
+  }
+
+  // init timeline
+  const correspondContainer = document.getElementById('correspondence')
+  const timelineContainer = document.getElementById('timeline')
+  if (correspondContainer && timelineContainer) {
+    new Timeline(
+      correspondContainer, timelineContainer, messages
+    )
+  }
+}
+
+
 const initMessageContainers = () => {
   const messages: Message[] = []
   const urlHash = window.location.hash
@@ -14,36 +45,6 @@ const initMessageContainers = () => {
     messages.push(new Message(el as HTMLElement, forceExpand as Boolean, isLastItem as Boolean))
   })
   return messages
-}
-
-
-const init = () => {
-  // init message containers
-  const messages: Message[] = initMessageContainers()
-
-  // when all messages initialized:
-  // scroll to comment if query parameters given (e.g. ?msg=76058&c=10856)
-  // find message and comment containers with id that equal query params
-  if (messages.length) {
-    const urlParams = new URLSearchParams(window.location.search);
-    const msgParam = urlParams.get('msg')
-    const commentParam = urlParams.get('c')
-    const scrollToMsgId = msgParam ? `nachricht-${msgParam}` : null
-    const scrollToCommentId = commentParam ? `comment-${commentParam}` : null
-    if (scrollToMsgId && scrollToCommentId) {
-      const msg = messages.find(m => m.id === scrollToMsgId)
-      if (msg) {
-        msg.scrollToComment(scrollToCommentId)
-      }
-    }
-  }
-
-  // init timeline
-  const correspRoot = document.getElementById('correspondence')
-  const timelineRoot = document.getElementById('timeline')
-  if (correspRoot && timelineRoot) {
-
-  }
 }
 
 
