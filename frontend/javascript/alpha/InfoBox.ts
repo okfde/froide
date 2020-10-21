ffe8b223b7cdecc312f6ffb56681d0ddfe8c2fcb
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2020-10-21 17:51:08 +0200

    wip

---
+++ frontend/javascript/alpha/InfoBox.ts
@@ -0,0 +1,30 @@
+import { Event } from "@uppy/core"
+
+export default class InfoBox {
+  element: HTMLElement
+  editButton: HTMLElement
+  editPanel: HTMLElement
+  infoList: HTMLElement
+  editPanelIsVisible: boolean
+
+  constructor (infoboxContainer: HTMLElement) {
+    this.element = infoboxContainer
+    this.editButton = infoboxContainer.querySelector('.info-box__edit-button') as HTMLElement
+    this.editPanel = infoboxContainer.querySelector('.info-box__edit-panel') as HTMLElement
+    this.editPanelIsVisible = false
+    this.infoList = infoboxContainer.querySelector('.info-box__list') as HTMLElement
+
+    // event listeners
+    this.editButton.addEventListener('click', this.editButtonClickCallback.bind(this))
+  }
+
+  editButtonClickCallback (e: MouseEvent) {
+    e.preventDefault()
+    this.editPanel.classList.toggle('d-none')
+    this.infoList.classList.toggle('d-none')
+    this.editButton.children[0].classList.toggle('d-none')
+    this.editButton.children[1].classList.toggle('d-none')
+    this.editPanelIsVisible = !this.editPanelIsVisible
+  }
+
+}
--- frontend/javascript/alpha/request-alpha.ts
+++ frontend/javascript/alpha/request-alpha.ts
@@ -2,13 +2,51 @@ import '../../styles/components/alpha/index.scss'
 import Message from './Message'
 import Timeline from './Timeline'
 import ScrollIndicator from './ScrollIndicator'
+import InfoBox from './InfoBox'
 
 
 const init = () => {
+  const messagesContainer = document.getElementById('correspondence')
+  const timelineContainer = document.getElementById('timeline')
+  const infoboxContainer = document.getElementById('infobox')
+  if (!messagesContainer || !timelineContainer || !infoboxContainer) {
+    return
+  }
+
   // init message containers
-  const messages: Message[] = initMessageContainers()
+  const messages: Message[] = parseMessageContainers()
   if (!messages.length) return
 
+  // init Info Box
+  new InfoBox(infoboxContainer)
+
+  // init timeline
+  new Timeline(messagesContainer, timelineContainer, messages)
+
+  // init ScrollIndicator
+  new ScrollIndicator(messagesContainer)
+
+  // if url query parameter found, scroll to comment next
+  scrollToComment(messages)
+
+}
+
+
+const parseMessageContainers = () => {
+  const messages: Message[] = []
+  const urlHash = window.location.hash
+  const collapsedMsgId = /^#nachricht-[0-9]+$/.test(urlHash) ? urlHash.substr(1) : null
+  const messageContainers = document.querySelectorAll('.alpha-message')
+
+  messageContainers.forEach((el, idx) => {
+    const isLastItem = idx === messageContainers.length  - 1
+    const forceExpand = collapsedMsgId && collapsedMsgId === el.id
+    messages.push(new Message(el as HTMLElement, forceExpand as Boolean, isLastItem as Boolean))
+  })
+  return messages
+}
+
+const scrollToComment = (messages: Message[]) => {
   // when all messages initialized:
   // scroll to comment if query parameters given (e.g. ?msg=76058&c=10856)
   // find message and comment containers with id that equal query params
@@ -23,38 +61,6 @@ const init = () => {
       msg.scrollToComment(scrollToCommentId)
     }
   }
-
-  const messagesContainer = document.getElementById('correspondence')
-  if (messagesContainer) {
-
-    // init timeline
-    const timelineContainer = document.getElementById('timeline')
-    if (timelineContainer) {
-      new Timeline(
-        messagesContainer, timelineContainer, messages
-      )
-    }
-
-    // init ScrollIndicator
-    new ScrollIndicator(messagesContainer)
-
-  }
-
-}
-
-
-const initMessageContainers = () => {
-  const messages: Message[] = []
-  const urlHash = window.location.hash
-  const collapsedMsgId = /^#nachricht-[0-9]+$/.test(urlHash) ? urlHash.substr(1) : null
-  const messageContainers = document.querySelectorAll('.alpha-message')
-
-  messageContainers.forEach((el, idx) => {
-    const isLastItem = idx === messageContainers.length  - 1
-    const forceExpand = collapsedMsgId && collapsedMsgId === el.id
-    messages.push(new Message(el as HTMLElement, forceExpand as Boolean, isLastItem as Boolean))
-  })
-  return messages
 }
 
 
