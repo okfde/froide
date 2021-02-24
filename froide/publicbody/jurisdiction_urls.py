781d196a4c7f9be47d018324642d30de8edca1a3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-02-24 11:09:04 +0000

    Fix jurisdiction URLs translation

--- froide/publicbody/jurisdiction_urls.py
+++ froide/publicbody/jurisdiction_urls.py
@@ -1,19 +1,16 @@
 from django.urls import path
 from django.urls import reverse
 from django.http import HttpResponseRedirect
-from django.utils.translation import gettext as _
+from django.utils.translation import pgettext_lazy
 
 from .views import show_jurisdiction
 
 
 urlpatterns = [
     path("", show_jurisdiction, name="publicbody-show_jurisdiction"),
-    # Translators: URL part
-    path("%s/" % _('entity'),
+    path(pgettext_lazy('url part', 'entity/'),
         lambda r, slug: HttpResponseRedirect(
-            reverse("publicbody-list", kwargs={'jurisdiction': slug})),
-        name='show-pb_jurisdiction'),
-    # Translators: URL part
-    path("%s/" % _('entities'), lambda r, slug: HttpResponseRedirect(
+            reverse("publicbody-list", kwargs={'jurisdiction': slug}))),
+    path(pgettext_lazy('url part', 'entities/'), lambda r, slug: HttpResponseRedirect(
             reverse("publicbody-list", kwargs={'jurisdiction': slug}))),
 ]
