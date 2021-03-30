22206712d37957fa92abfaee67757dbe63e3a73f
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-03-30 13:31:47 +0100

    Fix short URL regex

--- froide/foirequest/urls/__init__.py
+++ froide/foirequest/urls/__init__.py
@@ -38,7 +38,7 @@ urlpatterns = [
     # Translators: Short-request auth URL
     re_path(pgettext_lazy('url part', r"^r/(?P<obj_id>\d+)/auth/(?P<code>[0-9a-f]+)/$"),
         auth, name="foirequest-auth"),
-    re_path(pgettext_lazy('url part', r"^r/(?P<obj_id>\d+)(?P<url_path>[\w/\-/]+)$"), shortlink,
+    re_path(pgettext_lazy('url part', r"^r/(?P<obj_id>\d+)(?P<url_path>/[\w/\-/]+)$"), shortlink,
         name="foirequest-shortlink_url"),
     re_path(pgettext_lazy('url part', r"^r/(?P<obj_id>\d+)"), shortlink,
         name="foirequest-shortlink"),
