8d4c83d1655cd20ebede247d3e1694f3b1374df3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-05-26 09:38:18 +0100

    Fix tree filter to include self

--- froide/helper/admin_utils.py
+++ froide/helper/admin_utils.py
@@ -391,7 +391,7 @@ class TreeRelatedFieldListFilter(admin.RelatedFieldListFilter):
             # #### MPTT ADDITION START
             if self.lookup_val:
                 other_model = self.other_model.objects.get(pk=self.lookup_val)
-                other_models = other_model.get_descendants()
+                other_models = other_model.__class__.get_tree(other_model)
                 del self.used_parameters[self.changed_lookup_kwarg]
                 self.used_parameters.update(
                     {'%s__%s__in' % (self.field_path, self.rel_name): other_models}
