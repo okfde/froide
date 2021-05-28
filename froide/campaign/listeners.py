67bcacb60a9e24970345d5f6daf3ba3649677b5c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2021-05-28 10:02:42 +0100

    Allow connecting froide_campaigns to campaign app

--- froide/campaign/listeners.py
+++ froide/campaign/listeners.py
@@ -5,7 +5,10 @@ def connect_campaign(sender, **kwargs):
     reference = kwargs.get('reference')
     if not reference:
         return
-    parts = reference.split(':', 1)
+    if '@' in reference:
+        parts = reference.split('@', 1)
+    else:
+        parts = reference.split(':', 1)
     if len(parts) != 2:
         return
     namespace = parts[0]
