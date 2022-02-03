e13c63d2021ddbd8c64787a40a079304434377f3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-02-03 19:18:55 +0000

    Override isort model import order
    Allows importing FoiRequest/FoiMessage to event module.
    This could be done with forward references, but this seems nicer.

--- froide/foirequest/models/__init__.py
+++ froide/foirequest/models/__init__.py
@@ -1,11 +1,12 @@
 from .attachment import FoiAttachment
 from .deferred import DeferredMessage
 from .draft import RequestDraft
-from .event import FoiEvent
 from .message import DeliveryStatus, FoiMessage, MessageTag, TaggedMessage
 from .project import FoiProject
 from .request import FoiRequest, TaggedFoiRequest
 from .suggestion import PublicBodySuggestion
+
+from .event import FoiEvent  # isort: skip
 
 __all__ = [
     "FoiRequest",
