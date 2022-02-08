89c93f70ed1f2a6b095380baf34e370fa3d42cab
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-02-08 11:32:34 +0000

    Add mfa tests for password reset / autologin urls

--- froide/account/tests/test_mfa.py
+++ froide/account/tests/test_mfa.py
@@ -1,7 +1,9 @@
+import re
 from datetime import timedelta
 from unittest import mock
 
 from django.contrib.auth import get_user_model
+from django.core import mail
 from django.test import TestCase
 from django.urls import reverse
 
@@ -46,3 +48,48 @@ class MFATest(TestCase):
             response = self.client.get(reverse("oauth2_provider:list"))
         self.assertEqual(response.status_code, 302)
         self.assertIn(reverse("account-reauth"), response["Location"])
+
+    def test_password_reset_mfa_no_login(self):
+        data = {"pwreset-email": self.test_user.email}
+        response = self.client.post(reverse("account-send_reset_password_link"), data)
+        self.assertEqual(response.status_code, 302)
+        message = mail.outbox[0]
+        match = re.search(r"/account/reset/([^/]+)/([^/]+)/", message.body)
+        uidb64 = match.group(1)
+        token = match.group(2)
+        response = self.client.get(
+            reverse(
+                "account-password_reset_confirm",
+                kwargs={"uidb64": uidb64, "token": token},
+            ),
+            follow=True,
+        )
+        self.assertEqual(response.status_code, 200)
+        data = {"new_password1": "froide4froide5", "new_password2": "froide4froide5"}
+        response = self.client.post(response.wsgi_request.path, data)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(
+            reverse("mfa:auth", kwargs={"method": "recovery"}), response["Location"]
+        )
+        # We are not logged in
+        response = self.client.get(reverse("account-requests"))
+        self.assertEqual(response.status_code, 302)
+
+    def test_go_with_mfa(self):
+        # test url is not cached and does not cause 404
+        test_url = reverse("account-requests")
+
+        # Try logging in via link: success
+        autologin = self.test_user.get_autologin_url(test_url)
+        response = self.client.get(autologin)
+        self.assertEqual(response.status_code, 200)
+        response = self.client.post(autologin)
+        self.assertEqual(response.status_code, 302)
+
+        self.assertIn(
+            reverse("mfa:auth", kwargs={"method": "recovery"}), response["Location"]
+        )
+
+        # We are not logged in
+        response = self.client.get(test_url)
+        self.assertEqual(response.status_code, 302)
