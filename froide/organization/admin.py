b6bedf581aed10877ee0cf9b887f99bb2bea85b6
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-02-15 17:59:25 +0100

    Add organization app

---
+++ froide/organization/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Organization, OrganizationMembership
+
+
+class OrganizationMembershipInline(admin.StackedInline):
+    model = OrganizationMembership
+    raw_id_fields = ("user",)
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "website", "created", "member_count")
+    inlines = [OrganizationMembershipInline]
+
+
+admin.site.register(Organization, OrganizationAdmin)
