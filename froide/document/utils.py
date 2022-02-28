23ed23323b52b29baf558ee28a2864dc774d01b9
missgreenwood <obermaedchen@gmail.com>
2022-02-28 11:30:24 +0100

    Add typing to module froide.document's util functions.

--- froide/document/utils.py
+++ froide/document/utils.py
@@ -1,6 +1,8 @@
 from froide.helper.tasks import search_instance_save
 
+from .models import Document
 
-def update_document_index(document):
+
+def update_document_index(document: Document) -> None:
     for pk in document.pages.all().values_list("id", flat=True):
         search_instance_save.delay("filingcabinet.page", pk)
