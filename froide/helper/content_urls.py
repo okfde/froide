7b556e803d5169ad66090e877e48ce95646f23b7
missgreenwood <obermaedchen@gmail.com>
2022-03-24 17:37:07 +0100

    Add typing to module froide.helper.content_urls.

--- froide/helper/content_urls.py
+++ froide/helper/content_urls.py
@@ -1,7 +1,10 @@
+from typing import Union
+
 from django.conf import settings
+from django.utils.safestring import SafeString
 
 CONTENT_URLS = settings.FROIDE_CONFIG.get("content_urls", {})
 
 
-def get_content_url(name):
+def get_content_url(name: Union[SafeString, str]) -> str:
     return CONTENT_URLS.get(name, "/")
