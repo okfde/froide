09cfd015f66f786a0c07fbbdd0d5ec190115ba1c
missgreenwood <obermaedchen@gmail.com>
2022-03-24 17:36:00 +0100

    Add typing to module froide.helper.cache.

--- froide/helper/cache.py
+++ froide/helper/cache.py
@@ -1,9 +1,16 @@
+from typing import Callable, Union
+
 from django.contrib.messages import get_messages
+from django.http import HttpRequest
+from django.http.response import HttpResponse
 from django.middleware.cache import CacheMiddleware
 from django.utils.decorators import decorator_from_middleware_with_args
 
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-def cache_anonymous_page(time, **cache_kwargs):
+
+def cache_anonymous_page(time: int, **cache_kwargs) -> Callable:
     """
     Like cache_page decorator, but only for not authenticated users
     """
@@ -20,14 +27,18 @@ def cache_anonymous_page(time, **cache_kwargs):
 
 
 class MessageAwareCacheMiddleware(CacheMiddleware):
-    def _should_update_cache(self, request, response):
+    def _should_update_cache(
+        self,
+        request: Union[HttpRequest, Request],
+        response: Union[Response, HttpResponse],
+    ) -> bool:
         should = super(MessageAwareCacheMiddleware, self)._should_update_cache(
             request, response
         )
         return should and not get_messages(request)
 
 
-def cache_page(timeout, cache=None, key_prefix=None):
+def cache_page(timeout: int, cache: None = None, key_prefix: None = None) -> Callable:
     return decorator_from_middleware_with_args(MessageAwareCacheMiddleware)(
         cache_timeout=timeout, cache_alias=cache, key_prefix=key_prefix
     )
