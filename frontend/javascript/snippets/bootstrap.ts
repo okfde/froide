8fb884a39f88a1811227b2cb28df77385146ae2e
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-08-18 14:50:21 +0100

    Import bootstrap component from dist module

--- frontend/javascript/snippets/bootstrap.ts
+++ frontend/javascript/snippets/bootstrap.ts
@@ -1,3 +1,6 @@
-import { Modal, Dropdown, Collapse, Alert, Tab, Tooltip } from 'bootstrap'
-
-export { Modal, Dropdown, Collapse, Alert, Tab, Tooltip }
+import 'bootstrap/js/dist/alert'
+import 'bootstrap/js/dist/collapse'
+import 'bootstrap/js/dist/dropdown'
+import 'bootstrap/js/dist/modal'
+import 'bootstrap/js/dist/tab'
+import 'bootstrap/js/dist/tooltip'
