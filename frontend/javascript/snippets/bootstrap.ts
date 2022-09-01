924b08e626e41f41b76f1dcf99bc9d0b4c398b2d
krmax44 <hi@krmax44.de>
2022-09-01 14:37:33 +0200

    🚧  fix some bootstrap js components

--- frontend/javascript/snippets/bootstrap.ts
+++ frontend/javascript/snippets/bootstrap.ts
@@ -1,6 +1,17 @@
-import 'bootstrap/js/dist/alert'
+// FIXME: init consistently across components, some don't work yet
 import 'bootstrap/js/dist/collapse'
-import 'bootstrap/js/dist/dropdown'
-import 'bootstrap/js/dist/modal'
 import 'bootstrap/js/dist/tab'
-import 'bootstrap/js/dist/tooltip'
+import 'bootstrap/js/dist/modal'
+import { Tooltip, Dropdown, Alert } from 'bootstrap'
+
+const selectors = {
+  '[data-bs-toggle="tooltip"]': Tooltip,
+  '[data-bs-toggle="dropdown"]': Dropdown,
+  '.alert': Alert
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  for (const [selector, Component] of Object.entries(selectors)) {
+    document.querySelectorAll(selector).forEach((el) => new Component(el))
+  }
+})
