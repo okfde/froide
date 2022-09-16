3a5f794db15c255e782d8461d2c3bd1364378331
Magdalena Noffke <magdalena.noffke@googlemail.com>
2022-09-16 15:29:59 +0200

    Update form

--- froide/comments/forms.py
+++ froide/comments/forms.py
@@ -18,3 +18,16 @@ class CommentForm(DjangoCommentForm):
         widget=forms.Textarea(attrs={"class": "form-control", "rows": "4"}),
         max_length=COMMENT_MAX_LENGTH,
     )
+    is_moderation = forms.BooleanField(
+        widget=forms.CheckboxInput(attrs={"class": "form-check-input"}),
+        required=False,
+        initial=True,
+        label=_("This is a moderation comment"),
+    )
+
+    def __init__(self, target_object, data=None, initial=None, **kwargs):
+        super().__init__(target_object, data=data, initial=initial, **kwargs)
+        object_id = self.target_object.id
+        for field in self.fields:
+            id = "id_{}_{}".format(field, object_id)
+            self.fields[field].widget.attrs["id"] = id
