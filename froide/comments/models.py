d752c2002d5461bfff7bdfca657cd8caa2f5c75f
Magdalena Noffke <magdalena.noffke@googlemail.com>
2022-09-19 15:49:46 +0200

    Add permission check

--- froide/comments/models.py
+++ froide/comments/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django_comments.abstracts import CommentAbstractModel
 from django_comments.signals import comment_will_be_posted
 
+from froide.foirequest.auth import can_moderate_foirequest
+
 
 class FroideComment(CommentAbstractModel):
     ip_address = None
@@ -10,7 +12,11 @@ class FroideComment(CommentAbstractModel):
 
 
 def set_is_moderation_flag(comment, **kwargs):
-    is_moderation = kwargs.get("request").POST.get("is_moderation") == "on"
+    is_moderation = False
+    request = kwargs.get("request")
+    foi_request = comment.content_object.request
+    if can_moderate_foirequest(foi_request, request):
+        is_moderation = request.POST.get("is_moderation") == "on"
     comment.is_moderation = is_moderation
 
 
