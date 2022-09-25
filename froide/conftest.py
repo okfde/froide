7dd7838126ea6f1ad7d0d14e72ef95e598f7d4fb
Magdalena Noffke <magdalena.noffke@googlemail.com>
2022-09-25 20:50:37 +0200

    Use pytest in account tests

--- froide/conftest.py
+++ froide/conftest.py
@@ -1,5 +1,20 @@
+import pytest
 from pytest_factoryboy import register
 
-from froide.foirequest.tests.factories import UserFactory
+from froide.foirequest.tests.factories import (
+    FoiRequestFactory,
+    RequestDraftFactory,
+    UserFactory,
+    make_world,
+)
+from froide.foirequestfollower.tests import FoiRequestFollowerFactory
 
 register(UserFactory)
+register(RequestDraftFactory)
+register(FoiRequestFactory)
+register(FoiRequestFollowerFactory)
+
+
+@pytest.fixture
+def world():
+    return make_world()
