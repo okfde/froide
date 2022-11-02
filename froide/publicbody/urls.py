8d7895fc334bfa65119d4268147adaf6d063b0d5
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2022-11-02 11:18:53 +0000

    Add moderation public body admin URL

--- froide/publicbody/urls.py
+++ froide/publicbody/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path, re_path
 from django.utils.translation import pgettext_lazy
 from django.views.generic.base import RedirectView
 
+from .admin import pb_admin_site
 from .views import (
     PublicBodyAcceptProposalView,
     PublicBodyChangeProposalView,
@@ -12,6 +13,7 @@ from .views import (
 )
 
 entities_url_patterns = [
+    path(pgettext_lazy("url part", "admin/"), pb_admin_site.urls),
     path(
         pgettext_lazy("url part", "propose/"),
         PublicBodyProposalView.as_view(),
