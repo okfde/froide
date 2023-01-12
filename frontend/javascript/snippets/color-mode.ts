bce94f5ace8f2dd03712c4c03415d7efa007949b
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2023-01-12 15:29:57 +0100

    Store selected color theme in localstorage

--- frontend/javascript/snippets/color-mode.ts
+++ frontend/javascript/snippets/color-mode.ts
@@ -1,17 +1,44 @@
-const setTheme = (theme: string): void => {
-  if (
-    theme === 'auto' &&
-    window.matchMedia('(prefers-color-scheme: dark)').matches
-  ) {
-    document.documentElement.setAttribute('data-bs-theme', 'dark')
-  } else {
-    document.documentElement.setAttribute('data-bs-theme', theme)
+const storageKey = 'colortheme'
+const storedTheme = localStorage.getItem(storageKey)
+
+const getPreferredTheme = (): string => {
+  if (storedTheme !== null) {
+    return storedTheme
   }
+
+  return 'light'
 }
 
 const getCurrentTheme = (): string | null => {
   return document.documentElement.getAttribute('data-bs-theme')
 }
+
+const toggle = document.getElementById('darkmode-toggle')
+
+const setTheme = (theme: string): void => {
+  const currentTheme = getCurrentTheme()
+  if (currentTheme === null) {
+    return
+  }
+
+  if (toggle === null) {
+    return
+  }
+  const icon = toggle.querySelector('i')
+  if (icon === null) {
+    return
+  }
+  if (theme === 'dark') {
+    document.documentElement.setAttribute('data-bs-theme', 'dark')
+    icon.classList.replace('fa-moon-o', 'fa-sun-o')
+  } else {
+    document.documentElement.setAttribute('data-bs-theme', theme)
+    icon.classList.replace('fa-sun-o', 'fa-moon-o')
+  }
+  localStorage.setItem(storageKey, theme)
+}
+
+setTheme(getPreferredTheme())
 
 window.addEventListener('DOMContentLoaded', () => {
   const defaultTheme = getCurrentTheme()
@@ -19,25 +46,17 @@ window.addEventListener('DOMContentLoaded', () => {
     return
   }
 
-  const toggle = document.getElementById('darkmode-toggle')
   if (toggle === null) {
     return
   }
-
   toggle.addEventListener('click', () => {
     const currentTheme = getCurrentTheme()
     if (currentTheme === null) {
       return
     }
-    const icon = toggle.querySelector('i')
-    if (icon === null) {
-      return
-    }
     if (currentTheme === 'dark') {
-      icon.classList.replace('fa-sun-o', 'fa-moon-o')
       setTheme('light')
     } else {
-      icon.classList.replace('fa-moon-o', 'fa-sun-o')
       setTheme('dark')
     }
   })
