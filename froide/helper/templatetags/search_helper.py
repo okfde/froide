08074368202b0f5e8cc76af343b9583124e1bc26
krmax44 <hi@krmax44.de>
2023-10-09 17:36:09 +0200

    🐛 handle pages without request context

--- froide/helper/templatetags/search_helper.py
+++ froide/helper/templatetags/search_helper.py
@@ -7,6 +7,9 @@ register = template.Library()
 
 @register.inclusion_tag("helper/search/search_list.html", takes_context=True)
 def render_search_list(context, current, num_results=None, query=""):
+    if context.get("request") is None:
+        return {}
+
     searches = search_registry.get_searches(context["request"])
     return {
         "current": current,
