428e33c151f8c03350b17beebe72f62563a1fd92
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2023-10-10 11:17:22 +0200

    Fix multi_search template tag when no request in context

--- froide/helper/templatetags/search_helper.py
+++ froide/helper/templatetags/search_helper.py
@@ -21,6 +21,9 @@ def render_search_list(context, current, num_results=None, query=""):
 
 @register.inclusion_tag("helper/search/multi_search.html", takes_context=True)
 def multi_search(context, small=False):
+    if context.get("request") is None:
+        return {}
+
     searches = search_registry.get_searches(context["request"])
 
     return {"searches": searches, "small": small}
