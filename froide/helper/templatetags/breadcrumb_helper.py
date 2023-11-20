1e73f89a7ccda3289598c3627c38ac19fc093bb5
krmax44 <hi@krmax44.de>
2023-11-20 17:47:28 +0100

    ✨ allow breadcrumbs to be dynamic

--- froide/helper/templatetags/breadcrumb_helper.py
+++ froide/helper/templatetags/breadcrumb_helper.py
@@ -1,8 +1,31 @@
 from django import template
+from django.urls import NoReverseMatch, reverse
 
 register = template.Library()
 
 
+def normalize_breadcrumb(breadcrumb):
+    if type(breadcrumb) == tuple:
+        if type(breadcrumb[1]) == str:
+            try:
+                breadcrumb = (breadcrumb[0], reverse(breadcrumb[1]))
+            except NoReverseMatch:
+                pass
+
+        return breadcrumb
+    else:
+        return (breadcrumb, None)
+
+
+@register.simple_tag()
+def get_breadcrumbs(view=None):
+    if hasattr(view, "get_breadcrumbs") and callable(view.get_breadcrumbs):
+        return view.get_breadcrumbs()
+
+    if hasattr(view, "breadcrumbs"):
+        return map(normalize_breadcrumb, view.breadcrumbs)
+
+
 @register.filter
-def is_tuple(value):
-    return type(value) == tuple
+def has_link(value):
+    return type(value) == tuple and len(value) == 2
