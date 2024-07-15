a8214ac1d6ddbe0337d102ca8b4cb6877b477c07
krmax44 <hi@krmax44.de>
2024-07-15 13:22:06 +0200

    ♻️ make breadcrumbs stricter

--- froide/helper/breadcrumbs.py
+++ froide/helper/breadcrumbs.py
@@ -1,14 +1,11 @@
 from __future__ import annotations
 
-import logging
-from dataclasses import dataclass
-from typing import Generator, Optional, Sequence, Tuple, Union
+import dataclasses
+from typing import Iterator, Optional, Sequence, Tuple, Union
 
 from django.template.context import Context
-from django.urls import NoReverseMatch, reverse
+from django.urls import reverse
 from django.views import View
-
-logger = logging.getLogger(__name__)
 
 BreadcrumbTuple = Tuple[str, Union[str, None]]
 """
@@ -27,7 +24,7 @@ class BreadcrumbView(View):
     The `breadcrumbs` attribute is a sequence of strings or tuples,
     e.g. ["Just text", ("A link", "url-pattern-name")].
     The first element of the tuple specifies the link text, the second one
-    will be REVERSED to the link, so you can provide path names.
+    will be REVERSED to the link, so you can provide path pattern names.
 
     Using the `get_breadcrumbs` method, you can provide breadcrumbs as
     above, with the difference that urls WILL NOT BE REVERSED.
@@ -43,33 +40,53 @@ class BreadcrumbView(View):
     breadcrumbs: Optional[BreadcrumbItems]
 
     def get_breadcrumbs(self) -> Union[BreadcrumbItems, Breadcrumbs]:
+        if hasattr(self, "breadcrumbs"):
+            # breadcrumbs in attributes are handled differently, see above
+            items = [
+                reverse_breadcrumb(normalize_breadcrumb(item))
+                for item in self.breadcrumbs
+            ]
+            return Breadcrumbs(items=items)
+
         raise NotImplementedError("No breadcrumb provider implemented")
 
 
-@dataclass
+@dataclasses.dataclass
 class BreadcrumbItem:
     # will be displayed as the link text
     title: str
     url: Optional[str] = None
-    # whether the breadcrumbs should overlay the following content
-    overlay: Optional[bool] = False
 
     @property
     def has_link(self) -> bool:
         return bool(self.url)
 
 
-@dataclass
+@dataclasses.dataclass
 class Breadcrumbs:
-    items: BreadcrumbItems
-    color: Optional[str] = None
+    """
+    You shouldn't iterate over Breadcrumbs.items, but over Breadcrumbs directly.
 
-    def __iter__(self) -> Generator[BreadcrumbItem]:
+    Example:
+
+    breadcrumbs = Breadcrumbs(items=...)
+    for breadcrumb in breadcrumbs:
+        ...
+    """
+
+    items: BreadcrumbItems
+    # background color of breadcrumbs
+    color: Optional[str] = None
+    # whether the breadcrumbs should overlay the following content
+    overlay: Optional[bool] = False
+
+    def __iter__(self) -> Iterator[BreadcrumbItem]:
         for item in self.items:
             if type(item) is str:
                 yield BreadcrumbItem(title=item)
             elif type(item) is tuple and len(item) == 2:
-                yield BreadcrumbItem(title=item[0], url=item[1])
+                # convert title to str, in case it's a lazy translation object
+                yield BreadcrumbItem(title=str(item[0]), url=item[1])
             else:
                 # i.e. lazy translation objects
                 yield BreadcrumbItem(title=str(item))
@@ -81,27 +98,19 @@ class Breadcrumbs:
         """
         Add items using the plus operator: breadcrumbs + [...]
         """
-        self.items += items
-        return self
+        return dataclasses.replace(self, items=self.items + items)
 
     @staticmethod
     def from_view(
         view: BreadcrumbView, context: Union[Context, dict[str, object]]
     ) -> Optional[Breadcrumbs]:
-        if hasattr(view, "get_breadcrumbs") and callable(view.get_breadcrumbs):
+        if isinstance(view, BreadcrumbView):
             items = view.get_breadcrumbs(context)
 
             if isinstance(items, Breadcrumbs):
                 return items
 
             return Breadcrumbs(items=[normalize_breadcrumb(item) for item in items])
-
-        if hasattr(view, "breadcrumbs"):
-            items = [
-                reverse_breadcrumb(normalize_breadcrumb(item))
-                for item in view.breadcrumbs
-            ]
-            return Breadcrumbs(items=items)
 
 
 def normalize_breadcrumb(breadcrumb: Union[str, BreadcrumbTuple]) -> BreadcrumbTuple:
@@ -110,15 +119,11 @@ def normalize_breadcrumb(breadcrumb: Union[str, BreadcrumbTuple]) -> BreadcrumbT
     elif type(breadcrumb) is str:
         return (breadcrumb, None)
 
-    logger.error("Received breadcrumb that is neither a tuple nor a string", breadcrumb)
+    raise TypeError("Breadcrumb must be either a tuple or a string")
 
 
 def reverse_breadcrumb(breadcrumb: BreadcrumbTuple) -> BreadcrumbTuple:
     if type(breadcrumb[1]) is str:
-        try:
-            return (breadcrumb[0], reverse(breadcrumb[1]))
-        except NoReverseMatch:
-            logger.error("Breadcrumb url could not be reversed", breadcrumb)
-            return (breadcrumb[0], None)
+        return (breadcrumb[0], reverse(breadcrumb[1]))
 
     return breadcrumb
