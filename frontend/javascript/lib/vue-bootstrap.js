a98ecc343bc4c9ec102910fc8d205bad78b24b42
Jakob Wierzba <git@jakobwierzba.de>
2024-11-07 18:41:39 +0100

    implement mobile friendly pdf-redaction with pan+pinch-zoom
    including
    - PDF rendering at higher dpi
    - vBsTooltip vs Options API
    - text mode redaction

--- frontend/javascript/lib/vue-bootstrap.js
+++ frontend/javascript/lib/vue-bootstrap.js
@@ -2,6 +2,7 @@ import { Tooltip } from 'bootstrap'
 
 /* alternatively, to register globally, we could add to vue-helpers.ts:
    (note also snippets/bootstrap.ts)
+   (see also redact.js for less global, Options API application)
   app.directive('bsTooltip', {
     mounted: (el) => {
       new Tooltip(el)
