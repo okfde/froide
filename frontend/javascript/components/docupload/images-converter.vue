446abae671eb14330c4aac61b629a29824c7b06c
Jakob Wierzba <git@jakobwierzba.de>
2024-11-18 15:31:27 +0100

    attachment manager WIP

---
+++ frontend/javascript/components/docupload/images-converter.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import { useAttachmentsStore } from './lib/attachments-store'
+import ImageDocumentPagesSortable from './image-document-pages-sortable.vue'
+
+const attachments = useAttachmentsStore()
+
+</script>
+
+<template>
+  <div v-for="image in attachments.images" :key="image.id">
+    <image-document-pages-sortable
+      :dense="simple"
+      :pages="image.pages"
+      @resorted="image.pages = $event"
+      @pageupdated="$emit('pageupdated', { document, ...$event })"
+      @splitpages="splitPages" />
+  </div>
+</template>
