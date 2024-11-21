c5869c06278afb000f56486e48f8fc74bb15c58e
Jakob Wierzba <git@jakobwierzba.de>
2024-11-21 19:06:22 +0100

    remove tooltips

--- frontend/javascript/redact.js
+++ frontend/javascript/redact.js
@@ -1,11 +1,9 @@
 import { createAppWithProps } from './lib/vue-helper'
-import { vBsTooltip } from './lib/vue-bootstrap'
 
 import PdfRedaction from './components/redaction/pdf-redaction'
 
 function createRedaction(selector) {
   const app = createAppWithProps(selector, PdfRedaction)
-  app.directive('bsTooltip', vBsTooltip)
   app.mount(selector)
 }
 
