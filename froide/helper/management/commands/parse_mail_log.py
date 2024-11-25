12e9257c843cd4106b483a5225681b222c798db7
Kara Engelhardt <kara.engelhardt@okfn.de>
2024-11-25 19:01:32 +0100

    ♻️ Replace pygtail with own alternative

--- froide/helper/management/commands/parse_mail_log.py
+++ froide/helper/management/commands/parse_mail_log.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils import translation
@@ -9,9 +11,9 @@ class Command(BaseCommand):
     help = "Processes mail logs for deliveries"
 
     def add_arguments(self, parser):
-        parser.add_argument("log_path")
-        parser.add_argument("offset_path")
+        parser.add_argument("log_path", type=Path, nargs="+")
+        parser.add_argument("offset_path", type=Path)
 
     def handle(self, *args, **options):
         translation.activate(settings.LANGUAGE_CODE)
-        check_delivery_from_log(str(options["log_path"]), str(options["offset_path"]))
+        check_delivery_from_log(options["log_path"], options["offset_path"])
