5fb9466862aa7f15cd63da8c7d4e91006521871f
Jakob Wierzba <git@jakobwierzba.de>
2024-12-10 08:06:59 +0100

    bootstrap modal as reusable component

---
+++ frontend/javascript/components/bs-modal.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { ref, onMounted, nextTick } from 'vue'
+import { Modal } from 'bootstrap'
+
+const props = defineProps({
+  dialogClasses: {
+    type: String,
+    default: ''
+  },
+  contentClasses: {
+    type: String,
+    default: ''
+  },
+  showOnMounted: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const modalEl = ref()
+let bsModal
+
+// Current behavior is "keep-alive"-ish:
+// modal markup will be rendered on first show,
+// then hidden, not unmounted.
+// Consider using a v-model here...
+const doRender = ref(false)
+
+const show = async () => {
+  if (!doRender.value) {
+    doRender.value = true
+    await nextTick()
+    initialize()
+  }
+  bsModal.show()
+}
+
+const hide = () => {
+  bsModal.hide()
+}
+
+const initialize = ()  => {
+  bsModal = new Modal(modalEl.value)
+}
+
+onMounted(() => {
+  if (props.showOnMounted) {
+    bsModal.show()
+  }
+})
+
+defineExpose({
+  show,
+  hide
+})
+</script>
+
+<template>
+  <Teleport to="body">
+    <div
+      ref="modalEl"
+      class="modal"
+      tabindex="-1"
+      role="dialog"
+      v-if="doRender"
+      >
+      <div
+        :class="'modal-dialog ' + dialogClasses"
+        role="document">
+        <div :class="'modal-content ' + contentClasses">
+          <div class="modal-header">
+            <slot name="header"></slot>
+            <button
+              @click="hide"
+              type="button"
+              class="btn-close"
+              aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <slot name="body"></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Teleport>
+</template>
