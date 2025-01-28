01b4409faf7825c2073692054669125ebd3563d1
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-01-28 10:27:44 +0100

    Use storage APIs in OverwriteStorage

--- froide/helper/storage.py
+++ froide/helper/storage.py
@@ -17,12 +17,11 @@ def sha256(file):
 
 
 class OverwriteStorage(FileSystemStorage):
+    # TODO: Remove in Django 5.1 in favour of allow_overwrite option
     def get_available_name(self, name, max_length=None):
         # FIXME: max_length is ignored
-        # If the filename already exists, remove it as if it was a true file system
         if self.exists(name):
-            full_path = self.path(name)
-            os.remove(full_path)
+            self.delete(name)
         return name
 
 
