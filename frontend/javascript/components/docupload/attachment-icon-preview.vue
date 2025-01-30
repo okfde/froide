2691dc1ec74c796bb3baafb44a95a6262cca70d1
Jakob Wierzba <git@jakobwierzba.de>
2025-01-30 16:29:16 +0100

    improve layout and selection

--- frontend/javascript/components/docupload/attachment-icon-preview.vue
+++ frontend/javascript/components/docupload/attachment-icon-preview.vue
@@ -8,14 +8,18 @@ import BsModal from '../bs-modal.vue'
 
 const i18n = inject('i18n')
 
-const { attachment, size } = defineProps({
+const { attachment, iconSize, thumbnailSize } = defineProps({
   attachment: {
     type: Object,
     required: true
   },
-  size: {
+  iconSize: {
     type: String,
     default: '2em'
+  },
+  thumbnailSize: {
+    type: String,
+    default: '4em'
   }
 })
 
@@ -28,22 +32,27 @@ const previewModal = ref()
     <a
       v-if="attachment.is_image"
       :href="attachment.site_url"
-      class="thumbnail"
+      class="d-block"
+      :style="{ width: thumbnailSize, height: thumbnailSize }"
       @click.prevent="previewModal.show()"
       >
-      <img :src="attachment.file_url" class="object-fit-contain shadow-sm me-2" />
+      <img
+        :src="attachment.file_url"
+        class="object-fit-contain shadow-sm"
+        :style="{ maxWidth: thumbnailSize, maxHeight: thumbnailSize }"
+        />
     </a>
     <a
       v-else
       :href="attachment.site_url"
-      class="btn btn-link p-0 me-2"
+      class="btn btn-link d-block p-0 me-2"
       type="button"
       @click.prevent="previewModal.show()">
-      <i class="fa fa-file" :style="{ fontSize: size }"></i>
+      <i class="fa fa-file" :style="{ fontSize: iconSize }"></i>
     </a>
     <bs-modal
       ref="previewModal"
-      dialog-classes="modal-dialog-scrollable ms-auto modal-xl"
+      dialog-classes="modal-dialog-scrollable ms-auto modal-xl modal-fullscreen-lg-down"
       content-classes="h-100"
       >
       <template #header>
@@ -51,7 +60,7 @@ const previewModal = ref()
       </template>
       <template #body>
         <div class="row h-100">
-          <div class="col-sm-8">
+          <div class="col-sm-8 mb-3">
             <img
               v-if="attachment.is_image"
               :src="attachment.file_url"
@@ -65,23 +74,26 @@ const previewModal = ref()
               class="h-100 w-100"
               ></iframe>
           </div>
-          <div class="actions col-sm-4 d-flex flex-column">
-            <div class="card mb-3">
+          <div class="col-sm-4 d-flex flex-column">
+            <div
+              v-if="attachment.size || attachment.filetype"
+              class="card mb-3"
+              >
               <div class="card-body">
                 <h5 class="card-title">
                   {{ i18n.properties }}
                 </h5>
-                <div>
+                <div v-if="attachment.size">
                   {{ i18n.size }}:
                   {{ Math.round(attachment.size / 1000) }}&nbsp;kB
                 </div>
-                <div>
+                <div v-if="attachment.filetype">
                   {{ i18n.fileType }}:
                   {{ attachment.filetype }}
                 </div>
               </div>
             </div>
-            <div class="card">
+            <div class="card actions">
               <div class="card-body">
                 <h5 class="card-title">
                   {{ i18n.actions }}
@@ -97,20 +109,7 @@ const previewModal = ref()
 </template>
 
 <style lang="scss" scoped>
-
-$icon-size: 4em;
-
 .actions :deep(.btn) {
   text-align: left;
-}
-
-.thumbnail {
-  width: $icon-size;
-  height: $icon-size;
-
-  img {
-    max-width: $icon-size;
-    max-height: $icon-size;
-  }
 }
 </style>
