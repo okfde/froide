fae2f87f80330b1d8c8a923a5ad20c231320bfa0
Jakob Wierzba <git@jakobwierzba.de>
2025-01-31 13:23:14 +0100

    add online help

--- froide/foirequest/views/message.py
+++ froide/foirequest/views/message.py
@@ -803,6 +803,7 @@ def upload_attachments(request, foirequest, message_id):
                 "foirequest-create_document",
                 kwargs={"slug": foirequest.slug, "attachment_id": 0},
             ),
+            "helpAttachmentsManagement": get_content_url("help_attachments_management"),
         },
         "i18n": {
             "attachmentName": _("Name of attachment to create"),
@@ -874,6 +875,7 @@ def upload_attachments(request, foirequest, message_id):
             "fileType": _("File type"),
             "actions": _("Actions"),
             "properties": _("Properties"),
+            "helpNeeded": _("I have technical difficulties / I need help"),
             "uppy": get_uppy_i18n(),
         },
     }
