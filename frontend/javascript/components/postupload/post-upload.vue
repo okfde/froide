e7d787f7688ed8ea12d5faf7c2aefb8d0b49dc22
Jakob Wierzba <git@jakobwierzba.de>
2025-02-04 09:06:27 +0100

    hide actions when flow-breaking; minor fixes

--- frontend/javascript/components/postupload/post-upload.vue
+++ frontend/javascript/components/postupload/post-upload.vue
@@ -251,6 +251,7 @@ const fileUploaderSucceeded = (uppyResult) => {
     })
 }
 
+// mark new attachments "auto_approve"
 const attachmentsAutoApproveSelection = ref({})
 watch(() => attachments.relevant, () => {
   attachments.relevant.forEach(att => {
@@ -1300,7 +1301,7 @@ addEventListener('hashchange', () => {
             <template #after-card="slotProps">
               <label class="text-center">
                 <input type="checkbox" v-model="attachmentsAutoApproveSelection[slotProps.attachment.id]" :value="true" />
-                veröffentlichen*
+                {{ i18n.publish }}*
               </label>
             </template>
             <template #after-table>
