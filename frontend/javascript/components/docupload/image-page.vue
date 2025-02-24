d05a4ca7faf20d419c653c5eb8ced7817eb15e91
Jakob Wierzba <git@jakobwierzba.de>
2025-02-24 17:35:41 +0100

    vue3ify image-page, fix regressions (exifsupport, split pages position off)

--- frontend/javascript/components/docupload/image-page.vue
+++ frontend/javascript/components/docupload/image-page.vue
@@ -1,3 +1,107 @@
+<script setup>
+import { computed, ref } from 'vue'
+
+import ExifReader from 'exifreader'
+
+import { useExifSupport } from './lib/exif-support'
+import { useAttachments } from './lib/attachments'
+
+const { hasExifSupport } = useExifSupport()
+const { splitPages, rotatePage } = useAttachments()
+
+const { idx, page, pageNum, pageCount, showRotate, showSplit } = defineProps({
+  idx: {
+    type: Number,
+    required: true
+  },
+  page: {
+    type: Object,
+    required: true
+  },
+  pageNum: {
+    type: Number,
+    required: true
+  },
+  pageCount: {
+    type: Number,
+    required: true
+  },
+  showRotate: {
+    type: Boolean,
+    default: false
+  },
+  showSplit: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const loaded = ref(false)
+const imgBitmap = ref(null)
+
+const totalRotate = computed(() => {
+  const rotDegree = page.rotate || 0
+  if (hasExifSupport.value) {
+    return rotDegree
+  }
+  return rotDegree + (page.implicitRotate || 0)
+})
+
+const isLast = computed(() => pageNum === pageCount)
+
+const progressAlmostComplete = computed(() =>
+  !progressUnknown.value && page.progress === page.progressTotal
+)
+
+const progressUnknown = computed(() => progressPercent.value === null)
+
+const progressPercent = computed(() => {
+  if (!page.progressTotal) {
+    return null
+  }
+  return (page.progress / page.progressTotal) * 100
+})
+
+const progressPercentLabel = computed(() => {
+  if (progressPercent.value) {
+    return `${progressPercent.value}%`
+  }
+  return '100%'
+})
+
+const fetchImage = () => {
+  fetch(page.file_url)
+    .then((response) => {
+      return response.blob()
+    })
+    .then((blob) => {
+      return Promise.all([blob.arrayBuffer(), createImageBitmap(blob)])
+    })
+    .then(([ab, v]) => {
+      const reader = ExifReader.load(ab, { expanded: true })
+      const data = {
+        exif: true,
+        width: v.width,
+        heigh: v.height,
+        implicitRotate: 0
+      }
+      const orientation = reader.exif?.Orientation?.value
+      if (orientation === 6) {
+        data.implicitRotate = 90
+      } else if (orientation === 8) {
+        data.implicitRotate = 270
+      } else if (orientation === 3) {
+        data.implicitRotate = 180
+      }
+      imgBitmap.value = v
+      loaded.value = true
+    })
+}
+
+fetchImage()
+
+</script>
+
 <template>
   <div class="page">
     <div class="page-frame d-flex align-items-center justify-content-center position-relative">
@@ -19,13 +123,13 @@
       <button
         v-if="showRotate"
         class="btn btn-sm text-body-secondary small"
-        @click="rotatePage">
+        @click="rotatePage(page)">
         <span class="fa fa-rotate-right" />
       </button>
       <button
         v-if="showSplit && !isLast"
         class="btn btn-sm text-body-secondary small ms-auto"
-        @click="splitPages">
+        @click="splitPages(idx, pageNum)">
         <span class="fa fa-scissors" />
       </button>
     </div>
@@ -48,119 +152,6 @@
     </div>
   </div>
 </template>
-
-<script>
-import ExifReader from 'exifreader'
-
-export default {
-  name: 'ImagePage',
-  props: {
-    page: {
-      type: Object,
-      required: true
-    },
-    pageNum: {
-      type: Number,
-      required: true
-    },
-    pageCount: {
-      type: Number,
-      required: true
-    },
-    showRotate: {
-      type: Boolean,
-      default: false
-    },
-    showSplit: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data() {
-    return {
-      loaded: false,
-      width: null,
-      height: null,
-      imgBitmap: null
-    }
-  },
-  created() {
-    this.fetchImage()
-  },
-  computed: {
-    totalRotate() {
-      const rotDegree = this.page.rotate || 0
-      if (this.$root.exifSupport) {
-        return rotDegree
-      }
-      return rotDegree + (this.page.implicitRotate || 0)
-    },
-    isLast() {
-      return this.pageNum === this.pageCount
-    },
-    progressAlmostComplete() {
-      return (
-        !this.progressUnknown && this.page.progress === this.page.progressTotal
-      )
-    },
-    progressUnknown() {
-      return this.progressPercent === null
-    },
-    progressPercent() {
-      if (!this.page.progressTotal) {
-        return null
-      }
-      return (this.page.progress / this.page.progressTotal) * 100
-    },
-    progressPercentLabel() {
-      if (this.progressPercent) {
-        return `${this.progressPercent}%`
-      }
-      return '100%'
-    }
-  },
-  methods: {
-    splitPages() {
-      this.$emit('splitpages')
-    },
-    rotatePage() {
-      this.$emit('rotatepage')
-    },
-    fetchImage() {
-      fetch(this.page.file_url)
-        .then((response) => {
-          return response.blob()
-        })
-        .then((blob) => {
-          return Promise.all([blob.arrayBuffer(), createImageBitmap(blob)])
-        })
-        .then(([ab, v]) => {
-          const reader = ExifReader.load(ab, { expanded: true })
-          const data = {
-            exif: true,
-            width: v.width,
-            heigh: v.height,
-            implicitRotate: 0
-          }
-          const orientation = reader.exif?.Orientation?.value
-          if (orientation === 6) {
-            data.implicitRotate = 90
-          } else if (orientation === 8) {
-            data.implicitRotate = 270
-          } else if (orientation === 3) {
-            data.implicitRotate = 180
-          }
-          this.$emit('pageupdated', {
-            pageNum: this.pageNum,
-            data
-          })
-          this.imgBitmap = v
-          this.loaded = true
-        })
-    }
-  }
-}
-</script>
 
 <style lang="scss" scoped>
 .page {
