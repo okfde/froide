d8a4302761195d69458bd8eca154ae0e3e64e0ef
Jakob Wierzba <git@jakobwierzba.de>
2025-03-10 16:48:12 +0100

    use document viewer in attachment preview

---
+++ frontend/javascript/components/docupload/document-viewer.vue
@@ -0,0 +1,48 @@
+<!-- the purpose of this component is to provide $root.config, specifically .i18n,
+  to filingcabinet's document-viewer and its descendants -->
+<template>
+  <document-viewer
+    :document-url="this.documentUrl"
+    :document-preview="this.documentPreview"
+    :page="this.page"
+    :config="this.config"
+    :defaults="this.defaults"
+    ></document-viewer>
+</template>
+
+<script>
+
+import DocumentViewer from '@okfde/filingcabinet/frontend/javascript/components/document-viewer.vue'
+
+export default {
+  components: {
+    DocumentViewer
+  },
+  props: {
+    documentUrl: {
+      type: String,
+      required: true
+    },
+    documentPreview: {
+      type: Object,
+      default: null
+    },
+    page: {
+      type: Number,
+      default: 1
+    },
+    config: {
+      type: Object,
+      default: () => ({})
+    },
+    defaults: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  created() {
+    this.$root.config = this.config
+  }
+}
+
+</script>
