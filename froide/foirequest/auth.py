605b2c6d9ab5b730089dd216014febcadfde31a0
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-03-21 15:03:46 +0100

    Add write:request OAuth scope

--- froide/foirequest/auth.py
+++ froide/foirequest/auth.py
@@ -187,10 +187,8 @@ def can_read_foiproject_authenticated(
 
 
 @lru_cache()
-def can_write_foirequest(
-    foirequest: FoiRequest, request: HttpRequest, scope=None
-) -> bool:
-    if can_write_object(foirequest, request, scope):
+def can_write_foirequest(foirequest: FoiRequest, request: HttpRequest) -> bool:
+    if can_write_object(foirequest, request, scope="write:request"):
         return True
 
     if foirequest.project:
