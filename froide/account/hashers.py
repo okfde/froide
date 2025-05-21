ba8b90d0e769072cc3948e91e49127ab03f4a948
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-05-21 12:09:21 +0200

    Replace Sha1PasswordHasher import with code
    In order to verify and upgrade legacy password hashes

--- froide/account/hashers.py
+++ froide/account/hashers.py
@@ -2,7 +2,9 @@
 From: https://docs.djangoproject.com/en/1.11/topics/auth/passwords/#password-upgrading-without-requiring-a-login
 """
 
-from django.contrib.auth.hashers import PBKDF2PasswordHasher, SHA1PasswordHasher
+import hashlib
+
+from django.contrib.auth.hashers import PBKDF2PasswordHasher
 
 
 class PBKDF2WrappedSHA1PasswordHasher(PBKDF2PasswordHasher):
@@ -14,5 +16,11 @@ class PBKDF2WrappedSHA1PasswordHasher(PBKDF2PasswordHasher):
         )
 
     def encode(self, password, salt, iterations=None):
-        _, _, sha1_hash = SHA1PasswordHasher().encode(password, salt).split("$", 2)
+        _, _, sha1_hash = self.sha1_encode(password, salt).split("$", 2)
         return self.encode_sha1_hash(sha1_hash, salt, iterations)
+
+    def sha1_encode(self, password, salt):
+        # Taken from https://github.com/django/django/blob/5.0.14/django/contrib/auth/hashers.py#L666
+        self._check_encode_args(password, salt)
+        hash = hashlib.sha1((salt + password).encode()).hexdigest()
+        return "%s$%s$%s" % (self.algorithm, salt, hash)
