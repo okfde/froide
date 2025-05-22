4bbcede7bdb2ab8bd3c1a9b7ca119451da3b63a7
Jakob Wierzba <git@jakobwierzba.de>
2025-05-22 12:33:16 +0200

    fix "approve unredacted"

--- frontend/javascript/components/redaction/pdf-redaction.vue
+++ frontend/javascript/components/redaction/pdf-redaction.vue
@@ -564,7 +564,7 @@ export default {
       this.tool = altKey ? 'move' : 'paint'
     }
   },
-  created() {
+  async created() {
     import('pdfjs-dist')
       .then((PDFJS) => {
         this.PDFJS = PDFJS
@@ -575,7 +575,7 @@ export default {
       .catch((err) => {
         console.log(err)
       })
-    this.attachment = fetchAttachment(this.attachmentId)
+    this.attachment = await fetchAttachment(this.attachmentId)
   },
   mounted() {
     document.addEventListener('keydown', this.setAltKey)
