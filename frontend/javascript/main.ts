bbacc9e0de5fe2363a93b4adb9eba985f5c77e12
krmax44 <hi@krmax44.de>
2025-06-19 13:27:44 +0200

    ♻️ make search esm instead of global import

--- frontend/javascript/main.ts
+++ frontend/javascript/main.ts
@@ -1,10 +1,15 @@
 import '../styles/main.scss'
 
+// TODO: refactor global imports to ESM, like with search
 import './snippets/bootstrap.ts'
 import './snippets/copy-text'
 import './snippets/form-ajax.ts'
 import './snippets/inline-edit-forms.ts'
 import './snippets/misc.ts'
-import './snippets/search.ts'
 import './snippets/share-links.ts'
 import './snippets/color-mode.ts'
+import { initSearch } from './snippets/search.ts'
+
+document.addEventListener('DOMContentLoaded', () => {
+  initSearch()
+})
