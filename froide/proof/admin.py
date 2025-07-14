6b2a4598e08d3f9ed10c0c5061e31b648a5fadcd
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2025-07-14 09:50:14 +0200

    Annotate user's deterministic email on related lookup in admin search

--- froide/proof/admin.py
+++ froide/proof/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from froide.account.auth import MFAAndRecentAuthRequiredAdminMixin
+from froide.account.models import annotate_deterministic_field
 from froide.helper.admin_utils import ForeignKeyFilter
 
 from .models import Proof
@@ -12,7 +13,17 @@ class ProofAdmin(MFAAndRecentAuthRequiredAdminMixin, admin.ModelAdmin):
     list_display = ("user", "name", "timestamp")
     list_filter = (("user", ForeignKeyFilter),)
     exclude = ("key", "file")
-    search_fields = ("user__email_deterministic",)
+    search_fields = ("user_email_deterministic",)
 
     def has_change_permission(self, request, obj=None):
         return False
+
+    def get_queryset(self, request):
+        qs = (
+            super()
+            .get_queryset(request)
+            .annotate(
+                user_email_deterministic=annotate_deterministic_field("user__email")
+            )
+        )
+        return qs
