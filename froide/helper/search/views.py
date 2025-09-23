71bc63d4af569d90fa9959be427fefd9f04c9e80
Anett Seeker <noeddl@users.noreply.github.com>
2025-09-23 12:06:56 +0200

    💄 Clean up display of highlighted search results

--- froide/helper/search/views.py
+++ froide/helper/search/views.py
@@ -63,7 +63,11 @@ class BaseSearchView(ListView):
         if not self.has_query:
             s = s.sort(self.default_sort)
         else:
-            s = s.highlight_options(encoder="html").highlight("content")
+            # Retrieve 10 fragments of highlighted text, to be reduced to 5 later on
+            # due to possible duplicates.
+            s = s.highlight_options(encoder="html", number_of_fragments=10).highlight(
+                "content"
+            )
             s = s.sort("_score")
         return s
 
