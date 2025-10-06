3d55f4e08b406def0366e0f5aa6ada1a9b755af5
Jakob Wierzba <git@jakobwierzba.de>
2025-10-06 19:16:39 +0200

    refactor filter badge component

--- frontend/javascript/components/publicbody/pb-filter-selected.vue
+++ frontend/javascript/components/publicbody/pb-filter-selected.vue
@@ -1,37 +1,24 @@
 <template>
   <div v-if="hasValue">
-    <div v-for="v in valueList" :key="v.id" class="filter-badge">
-      <span class="text-truncate">{{ v.label }}</span>
-      <button
-        @click="removeFilter(v)"
-        type="button"
-        class="btn btn-close btn-close-white"
-        aria-label="Close"></button>
-    </div>
+    <PbFilterBadge
+      v-for="v in valueList"
+      :key="v.id"
+      :label="config.label"
+      :value="v.label"
+      @remove-click="() => removeFilter(v)"
+      />
   </div>
 </template>
 
 <script>
 import FilterMixin from './lib/filter-mixin'
+import PbFilterBadge from './pb-filter-badge'
 
 export default {
   name: 'PbFilterSelected',
+  components: { PbFilterBadge },
   props: ['globalConfig', 'config', 'i18n', 'scope', 'value', 'expanded'],
   mixins: [FilterMixin]
 }
 </script>
 
-<style lang="scss" scoped>
-@import '../../../styles/variables';
-
-.filter-badge {
-  background-color: $primary;
-  color: #fff;
-  display: block;
-  border-radius: 4px;
-  padding: 0.25rem 0.5rem;
-  margin: 0.5rem 0;
-  display: flex;
-  justify-content: space-between;
-}
-</style>
