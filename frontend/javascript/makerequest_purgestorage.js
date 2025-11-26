56d30a92c4aacf7fc39c511da95725ddddf10b70
Jakob Wierzba <git@jakobwierzba.de>
2025-11-26 19:50:43 +0100

    purge storage after logout

--- frontend/javascript/makerequest_purgestorage.js
+++ frontend/javascript/makerequest_purgestorage.js
@@ -6,4 +6,7 @@ import store from './store'
 // (scope: make-request-draft-123, see RequestPage.created)
 // but due to the nature of drafts this does not matter
 // (they're both more specific and ephemeral)
+
+// see also snippets/purgestorage.ts for logout behavior
+
 store.dispatch('purgeStorage', { scope: 'make-request' })
