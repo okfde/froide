bdfc40f050b648982340e28468d0d8c0e1c0aa6d
krmax44 <hi@krmax44.de>
2026-02-02 17:34:53 +0100

    💄 adjust visual hierarchy of campaigns

--- frontend/javascript/components/makerequest/intro-campaigns.vue
+++ frontend/javascript/components/makerequest/intro-campaigns.vue
@@ -2,23 +2,21 @@
   <div>
     <div class="row">
       <!-- col-12 is slightly akward on medium viewports but will make sure it is front and center -->
-      <div class="col-12 col-md-4 mb-4">
-        <div class="card h-100">
-          <div class="card-body d-flex flex-column">
-            <slot name="campaign_main"></slot>
-            <div class="mt-auto">
-              <button
-                type="button"
-                class="btn btn-primary w-100"
-                @click="emit('stepNext')"
-              >
-                {{ i18n.makeRequest }}
-              </button>
-            </div>
+      <div class="col-12 col-md-4 mb-4 d-flex">
+        <div class="d-flex flex-column border border-2 border-light h-100 p-3">
+          <slot name="campaign_main" />
+          <div class="mt-auto">
+            <button
+              type="button"
+              class="btn btn-primary w-100"
+              @click="emit('stepNext')"
+            >
+              {{ i18n.makeRequest }}
+            </button>
           </div>
         </div>
       </div>
-      <slot name="campaigns"></slot>
+      <slot name="campaigns" />
     </div>
   </div>
 </template>
@@ -37,3 +35,13 @@ defineProps({
 
 const emit = defineEmits('stepNext')
 </script>
+
+<style lang="scss" scoped>
+:deep(.campaign-logo) {
+  max-height: 6em;
+}
+
+:deep(ul) {
+  padding-left: 1rem;
+}
+</style>
