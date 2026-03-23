3db463539b729a0c183bdd48d852772183eb3b4b
krmax44 <hi@krmax44.de>
2026-03-23 16:55:19 +0100

    💄 make stepper smaller and more accessible

--- frontend/javascript/components/postupload/simple-stepper.vue
+++ frontend/javascript/components/postupload/simple-stepper.vue
@@ -47,40 +47,44 @@ const progressMobile = computed(() => (props.step + 1) / props.steps.length)
   <div class="simple-stepper">
     <!-- desktop/md -->
     <div class="d-none d-md-block py-2">
-      <div class="container">
-        <div class="row position-relative">
-          <component
-            :is="clickable && stepIndex <= maxClickableStep ? 'a' : 'div'"
-            v-for="(stepLabel, stepIndex) in steps"
-            @click.prevent="stepClick(stepIndex)"
-            :href="clickable ? '#step-' + stepIndex : false"
-            :key="stepIndex"
-            :class="{
-              'fw-bold': stepIndex <= step
-            }"
-            class="step col d-flex flex-column align-items-center text-body z-1 flex-grow-1 flex-shrink-1 text-break"
-          >
-            <div
-              :class="`step-marker ${markerClasses(stepIndex)} d-block rounded-circle text-center border border-primary`"
+      <nav class="container">
+        <div class="position-relative">
+          <ol class="d-flex justify-content-between list-unstyled m-0">
+            <li
+              v-for="(stepLabel, stepIndex) in steps"
+              :key="stepIndex"
+              class="d-contents"
             >
-              {{ stepIndex + 1 }}
-            </div>
-            <div class="text-center">{{ stepLabel }}</div>
-          </component>
-          <div
-            class="progress progress--desktop position-absolute translate-middle-y"
-            :style="{
-              width: 100 * (1 - 1 / props.steps.length) + '%',
-              left: 50 / props.steps.length + '%'
-            }"
-          >
+              <component
+                :is="clickable && stepIndex <= maxClickableStep ? 'a' : 'div'"
+                :href="clickable ? '#step-' + stepIndex : false"
+                class="step d-flex flex-column align-items-center text-body z-1 small"
+                :class="{
+                  'fw-semibold': stepIndex <= step
+                }"
+                :aria-current="stepIndex == step ? 'step' : null"
+                :aria-disabled="stepIndex > step"
+                @click.prevent="stepClick(stepIndex)"
+              >
+                <div
+                  class="step-marker d-block rounded-circle text-center border border-primary"
+                  :class="markerClasses(stepIndex)"
+                  aria-hidden="true"
+                >
+                  {{ stepIndex + 1 }}
+                </div>
+                <div class="text-center step-label">{{ stepLabel }}</div>
+              </component>
+            </li>
+          </ol>
+          <div class="progress progress--desktop position-absolute">
             <div
               class="progress-bar"
               :style="{ width: progressDesktop * 100 + '%' }"
             ></div>
           </div>
         </div>
-      </div>
+      </nav>
     </div>
     <!-- mobile -->
     <!-- the breadcrumbs fit well in post-upload but make this component less reusable -->
@@ -103,12 +107,23 @@ const progressMobile = computed(() => (props.step + 1) / props.steps.length)
 </template>
 
 <style lang="scss" scoped>
+@import '../../../styles/variables.scss';
+
 .step {
-  hyphens: auto;
+  width: 9rem;
+}
+
+a.step {
+  &:hover {
+    text-decoration: none;
+
+    .step-label {
+      text-decoration: underline;
+    }
+  }
 }
 
 .step-marker {
-  font-size: 1rem;
   width: 2rem;
   height: 2rem;
   line-height: 1.9rem;
@@ -120,5 +135,8 @@ const progressMobile = computed(() => (props.step + 1) / props.steps.length)
 
 .progress--desktop {
   top: 1rem;
+  left: 4.5rem;
+  right: 4.5rem;
+  width: calc(100% - 9rem);
 }
 </style>
