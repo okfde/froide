35d19d361426af53b6fc99d2cd94dfeed24ec56c
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2026-04-01 14:21:16 +0200

    Integrate public body contact model into proposal/accept flow

--- froide/publicbody/static/js/publicbody_changes.js
+++ froide/publicbody/static/js/publicbody_changes.js
@@ -10,18 +10,51 @@
       const fieldName = valueElement.dataset.field
       const value = valueElement.dataset.value
       const el = document.querySelector(`[name="${fieldName}"]`)
-      if (el.id && window._choices_[el.id]) {
+      if (el.id && window._choices_[el.id] && value) {
         const choicesInstance = window._choices_[el.id]
         const parsedValue = JSON.parse(value)
         choicesInstance.highlightAll().removeHighlightedItems()
         choicesInstance.setValue(parsedValue)
       } else {
-        el.value = value
+        if (value === "None") {
+          el.value = ""
+        } else {
+          el.value = value
+        }
       }
     })
   }
 
   Array.from(proposals).forEach((p) => {
     p.addEventListener('click', applyProposal)
+  })
+
+
+  const formsets = document.querySelectorAll('[data-formset]')
+  Array.from(formsets).forEach((formset) => {
+    const prefix = formset.dataset.formset
+    const template = formset.querySelector('[data-formsetform]')
+
+    const addButton = formset.querySelector('[data-formsetadd]')
+    addButton.addEventListener('click', () => {
+      const newForm = template.cloneNode(true)
+      const totalForms = formset.querySelector('[name$="-TOTAL_FORMS"]')
+      const formIndex = parseInt(totalForms.value, 10)
+      totalForms.value = formIndex + 1
+      
+      Array.from(newForm.querySelectorAll('input, select, textarea')).forEach((input) => {
+        if (input.name) {
+          input.name = input.name.replace(`${prefix}-0-`, `${prefix}-${formIndex}-`)
+        }
+        if (input.id) {
+          input.id = input.id.replace(`${prefix}-0-`, `${prefix}-${formIndex}-`)
+        }
+        if (input.for) {
+          input.for = input.for.replace(`${prefix}-0-`, `${prefix}-${formIndex}-`)
+        }
+        input.value = ""
+      })
+      formset.insertBefore(newForm, addButton)
+    })
   })
 })()
