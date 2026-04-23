b5116be521cbb5e3f0e77a3c5c3a3a39f15e9804
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2026-04-23 11:57:05 +0200

    Fix UserFactory warning

--- froide/account/factories.py
+++ froide/account/factories.py
@@ -5,16 +5,18 @@ from django.contrib.auth import get_user_model
 import factory
 from factory.django import DjangoModelFactory
 
+User = get_user_model()
+
 
 class UserFactory(DjangoModelFactory):
     class Meta:
-        model = get_user_model()
+        model = User
+        skip_postgeneration_save = True
 
     first_name = "Jane"
     last_name = factory.Sequence(lambda n: "D%se" % ("o" * min(20, int(n))))
     username = factory.Sequence(lambda n: "user_%s" % n)
     email = factory.Sequence(lambda n: "user%s@example.org" % n)
-    password = factory.PostGenerationMethodCall("set_password", "froide")
     is_staff = False
     is_active = True
     is_superuser = False
@@ -24,3 +26,9 @@ class UserFactory(DjangoModelFactory):
     address = "Dummystreet5\n12345 Town"
     organization_name = ""
     organization_url = ""
+
+    @factory.post_generation
+    def set_password(instance: User, create: bool, extracted, **kwargs):
+        if create:
+            instance.set_password("froide")
+            instance.save()
