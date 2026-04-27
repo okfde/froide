3952a0d472c12fcfc89d68f1abc7919dc510313d
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2026-04-27 16:46:06 +0200

    Activate search alerts

--- froide/searchalert/templatetags/searchalert_tags.py
+++ froide/searchalert/templatetags/searchalert_tags.py
@@ -1,8 +1,6 @@
 from django import template
 from django.template.loader import render_to_string
 
-from froide.helper.auth import is_crew
-
 from ..forms import AlertForm
 
 register = template.Library()
@@ -11,8 +9,6 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def search_alert_form(context, query=None, label=None):
     request = context["request"]
-    if not is_crew(request.user):
-        return ""
     initial = {"query": query or ""}
     context = {"form": AlertForm(request=request, initial=initial), "label": label}
     return render_to_string("searchalert/_alert_form.html", context, request)
