386ed7f8d0d446d28414e38ce9c50dc36d0b7360
krmax44 <hi@krmax44.de>
2026-06-04 11:24:10 +0200

    ♻️ remove unused `navigate` argument

--- froide/tests/live/utils.py
+++ froide/tests/live/utils.py
@@ -27,11 +27,10 @@ async def go_to_request_page(page, live_server, foirequest):
 
 
 @pytest.mark.asyncio(loop_scope="session")
-async def do_login(page, live_server, navigate=True):
-    if navigate:
-        await page.goto(live_server.url + reverse("account-login"))
-        user = User.objects.get(username="dummy")
-        await page.fill("[name=username]", user.email)
-        await page.fill("[name=password]", "froide")
-        await page.locator('button.btn.btn-primary[type="submit"]').click()
-        await expect(page.locator("#navbaraccount-link")).to_have_count(1)
+async def do_login(page, live_server):
+    await page.goto(live_server.url + reverse("account-login"))
+    user = User.objects.get(username="dummy")
+    await page.fill("[name=username]", user.email)
+    await page.fill("[name=password]", "froide")
+    await page.locator('button.btn.btn-primary[type="submit"]').click()
+    await expect(page.locator("#navbaraccount-link")).to_have_count(1)
