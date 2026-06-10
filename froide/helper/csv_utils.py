c22a4e43592c968d69ae5448a879f8432fda5a63
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2026-06-10 12:40:07 +0200

    Extend CSV utils to allow different encoding and delimiter

--- froide/helper/csv_utils.py
+++ froide/helper/csv_utils.py
@@ -1,6 +1,7 @@
 import csv
 import re
 from datetime import datetime
+from typing import Generator
 
 from django.http import StreamingHttpResponse
 
@@ -14,8 +15,21 @@ def export_csv_response(generator, name="export.csv"):
 
 
 class FakeFile(object):
-    def write(self, string):
-        self._last_string = string.encode("utf-8")
+    string_buffer: list[str]
+
+    def __init__(self, encoding="utf-8"):
+        self._encoding = encoding
+        self.string_buffer = []
+
+    def write(self, string: str):
+        self.string_buffer.append(string)
+
+    def read_bytes(self):
+        return_bytes = bytes().join(
+            s.encode(self._encoding) for s in self.string_buffer
+        )
+        self.string_buffer = []
+        return return_bytes
 
 
 def get_dict(obj, fields):
@@ -49,7 +63,7 @@ def export_csv(queryset, fields):
     yield from dict_to_csv_stream(export_dict_stream(queryset, fields))
 
 
-def export_dict_stream(queryset, fields):
+def export_dict_stream(queryset, fields) -> Generator[dict, None, None]:
     for obj in queryset:
         if hasattr(obj, "get_dict"):
             d = obj.get_dict(fields)
@@ -64,18 +78,20 @@ def sanitize_row(row):
     return row
 
 
-def dict_to_csv_stream(stream):
+def dict_to_csv_stream(
+    stream, encoding="utf-8", delimiter=","
+) -> Generator[bytes, None, None]:
     writer = None
-    fake_file = FakeFile()
+    fake_file = FakeFile(encoding=encoding)
     for d in stream:
         if writer is None:
             field_names = list(d.keys())
-            writer = csv.DictWriter(fake_file, field_names)
+            writer = csv.DictWriter(fake_file, field_names, delimiter=delimiter)
             writer.writeheader()
-            yield fake_file._last_string
+            yield fake_file.read_bytes()
         writer.writerow(sanitize_row(d))
-        yield fake_file._last_string
+        yield fake_file.read_bytes()
 
 
-def export_csv_bytes(generator):
+def export_csv_bytes(generator: Generator[bytes, None, None]) -> bytes:
     return bytes().join(generator)
