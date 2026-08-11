57685f4a23979281cc43a0140bbedfa217f8d70a
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2026-08-11 11:50:33 +0100

    Improve cleanup of websocket

--- frontend/javascript/lib/websocket.ts
+++ frontend/javascript/lib/websocket.ts
@@ -10,7 +10,7 @@ const RETRY_SECONDS = 3
 type AppData = Record<string, unknown>
 type Callback = (data: AppData) => void
 
-type CallbackMapping = Record<string, Callback[]>
+type CallbackMapping = Map<string, Callback[]>
 
 interface EventData {
   type: string
@@ -23,8 +23,8 @@ class Room {
   private retryInterval: number | null = null
   private heartBeatInterval: number | null = null
   private socketClosed = true
-  private callbacks: CallbackMapping = {}
-  private queue: EventData[] = []
+  private callbacks: CallbackMapping
+  private queue: EventData[]
   private onUnloadHandler: EventListener | null = null
   private readonly heartbeatSeconds: number
   private readonly retrySeconds: number
@@ -37,6 +37,8 @@ class Room {
     this.url = url
     this.heartbeatSeconds = heartbeatSeconds
     this.retrySeconds = retrySeconds
+    this.callbacks = new Map()
+    this.queue = []
 
     this.connect = this.connect.bind(this)
   }
@@ -49,7 +51,7 @@ class Room {
     this.socket = new WebSocket(`${prot}://${window.location.host}${this.url}`)
     this.clearRetry()
 
-    this.socket.onopen = () => {
+    this.socket.addEventListener('open', () => {
       this.socketClosed = false
       this.setupHeartbeat()
       this.queue.forEach((d) => {
@@ -57,26 +59,25 @@ class Room {
           this.socket.send(JSON.stringify(d))
         }
       })
+      this.queue = []
       // provide this binding to onunload; keep handle around for removing in onclose
       this.onUnloadHandler = () => this.onunload()
       window.addEventListener('beforeunload', this.onUnloadHandler)
-      this.queue = []
-    }
-    this.socket.onmessage = (e) => {
+    })
+    this.socket.addEventListener('message', (e) => {
       const data = JSON.parse(e.data) as EventData
       this.trigger(data.type, data)
-    }
-    this.socket.onerror = (e) => {
+    })
+    this.socket.addEventListener('error', (e) => {
       console.info('websocket error', e)
-    }
-    this.socket.onclose = () => {
-      this.clearHeartbeat()
-      window.removeEventListener('beforeunload', this.onUnloadHandler!)
+    })
+    this.socket.addEventListener('close', () => {
+      this.cleanup()
       if (!this.socketClosed) {
         console.info('Socket closed unexpectedly. Retrying...')
         this.setupRetry()
       }
-    }
+    })
     return this
   }
 
@@ -95,32 +96,58 @@ class Room {
   }
 
   on(event: string, callback: Callback): this {
-    this.callbacks[event] = this.callbacks[event] || []
-    this.callbacks[event].push(callback)
+    const cbs = this.callbacks.get(event)
+    if (cbs) {
+      cbs.push(callback)
+    } else {
+      this.callbacks.set(event, [callback])
+    }
     return this
   }
 
   off(event: string, callback: Callback): this {
-    this.callbacks[event] = this.callbacks[event] || []
-    this.callbacks[event] = this.callbacks[event].filter(
-      (cb) => cb !== callback
-    )
+    let cbs = this.callbacks.get(event) || []
+    cbs = cbs.filter((cb) => cb !== callback)
+    if (cbs.length === 0) {
+      this.callbacks.delete(event)
+    } else {
+      this.callbacks.set(event, cbs)
+    }
     return this
   }
 
   trigger(event: string, data: AppData): void {
-    if (!this.callbacks[event]) {
+    const cbs = this.callbacks.get(event)
+    if (cbs === undefined) {
       return
+    } else {
+      cbs.forEach((cb) => {
+        cb(data)
+      })
     }
-    this.callbacks[event].forEach((cb) => {
-      cb(data)
-    })
+  }
+
+  cleanup(): void {
+    if (this.onUnloadHandler) {
+      window.removeEventListener('beforeunload', this.onUnloadHandler)
+    }
+    this.clearHeartbeat()
+  }
+
+  close(): void {
+    this.cleanup()
+    this.clearRetry()
+    this.closeSocket()
   }
 
   closeSocket(): void {
     this.socketClosed = true
+    if (this.queue.length > 0) {
+      console.warn('Unsend messages in queue when closing socket')
+    }
     if (this.socket != null) {
       this.socket.close()
+      this.socket = null
     }
   }
 
@@ -129,10 +156,7 @@ class Room {
       if (this.socket != null && this.socket.readyState === 1) {
         this.socket.send(JSON.stringify({ type: 'heartbeat' }))
       } else {
-        if (this.heartBeatInterval) {
-          window.clearInterval(this.heartBeatInterval)
-          this.heartBeatInterval = null
-        }
+        this.clearHeartbeat()
       }
     }, this.heartbeatSeconds * 1000)
   }
