a622bdae13d9ddeb0c4379e92ad47fb337829b45
fin <fin+github@xbhd.org>
2026-08-16 18:35:13 +0000

    add MessageKind.FORM, test current MessageHandler functionality

--- froide/foirequest/models/message.py
+++ froide/foirequest/models/message.py
@@ -31,7 +31,7 @@ BULK_TAG = "bulk"
 class FoiMessageManager(models.Manager):
     def get_throttle_filter(self, queryset, user, extra_filters=None):
         qs = queryset.filter(
-            sender_user=user, is_response=False, kind=MessageKind.EMAIL
+            ~models.Q(kind=MessageKind.UPLOAD), sender_user=user, is_response=False
         )
         if extra_filters is not None:
             qs = qs.filter(**extra_filters)
@@ -68,6 +68,7 @@ class MessageKind(models.TextChoices):
     EMAIL = ("email", _("email"))
     POST = ("post", _("postal mail"))
     FAX = ("fax", _("fax"))
+    FORM = ("form", _("form"))
     # uploads by public bodies using link in foirequest
     UPLOAD = ("upload", _("upload"))
     PHONE = ("phone", _("phone call"))
@@ -87,6 +88,7 @@ MESSAGE_KIND_ICONS = {
     MessageKind.EMAIL: "mail",
     MessageKind.POST: "newspaper-o",
     MessageKind.FAX: "fax",
+    MessageKind.FORM: "file-text-o",
     # it's received, so the download icon seems more appropriate
     MessageKind.UPLOAD: "download",
     MessageKind.PHONE: "phone",
