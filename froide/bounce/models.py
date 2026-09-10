8cb1a113f883eebfa97f028a3b98d481fbbfb1ea
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2026-09-10 15:36:10 +0100

    Use random token in bounce and unsubscribe email local parts
    Value plus signature may exceed RFC 5321 defined local part max size of 64 bytes

--- froide/bounce/models.py
+++ froide/bounce/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
+from django.utils.crypto import get_random_string
 from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
@@ -35,6 +36,41 @@ class BounceManager(models.Manager):
                 email=email, user=user, bounces=[convert_bounce_info(bounce_info)]
             )
         return bounce
+
+
+class EmailTokenManager(models.Manager):
+    def _generate_token(cls):
+        # lowercase alphabet + digits only, 50 chars long, to be used in local part of email
+        # max length of local part is 64 chars, needs space for e.g. 'bounce+' prefix.
+        return get_random_string(
+            50, allowed_chars="abcdefghijklmnopqrstuvwxyz0123456789"
+        )
+
+    def get_token_for_email(self, email):
+        email_lower = email.lower()
+        contact, _created = self.get_or_create(
+            email=email_lower, defaults={"token": self._generate_token()}
+        )
+        return contact.token
+
+    def get_email_for_token(self, token):
+        contact = self.filter(token=token).first()
+        return contact.email if contact else None
+
+
+class EmailToken(models.Model):
+    email = models.EmailField(max_length=255, db_index=True, unique=True)
+    token = models.CharField(max_length=50, db_index=True, unique=True)
+    created = models.DateTimeField(default=timezone.now)
+
+    objects = EmailTokenManager()
+
+    class Meta:
+        verbose_name = _("Email Token")
+        verbose_name_plural = _("Email Tokens")
+
+    def __str__(self):
+        return self.email
 
 
 class Bounce(models.Model):
